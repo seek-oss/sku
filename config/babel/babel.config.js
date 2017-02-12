00d44f690c0e56c5ca169682bb334244e8ea25da
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2017-02-12 23:37:40 +1100

    Optimise React code for production build

--- config/babel/babel.config.js
+++ config/babel/babel.config.js
@@ -7,5 +7,12 @@ module.exports = {
   plugins: [
     require.resolve('babel-plugin-transform-class-properties'),
     require.resolve('babel-plugin-transform-object-rest-spread')
-  ]
+  ],
+  env: {
+    production: {
+      presets: [
+        require.resolve('babel-preset-react-optimize')
+      ]
+    }
+  }
 };

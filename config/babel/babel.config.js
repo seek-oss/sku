440a4d2bf860e9837199365ec87f492b391d7c31
Matt Jones <mjones@seek.com.au>
2017-06-30 11:13:33 +1000

    feat(babel): Add module directory resolution to babel

--- config/babel/babel.config.js
+++ config/babel/babel.config.js
@@ -2,7 +2,13 @@ module.exports = ({ webpack }) => {
   const es2015Options = webpack ? { modules: false } : {};
   const plugins = [
     require.resolve('babel-plugin-transform-class-properties'),
-    require.resolve('babel-plugin-transform-object-rest-spread')
+    require.resolve('babel-plugin-transform-object-rest-spread'),
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: [process.cwd()]
+      }
+    ]
   ];
 
   if (webpack) {

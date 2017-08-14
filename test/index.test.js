2603febb7cd0388cf7c51cc345dbf14d096e5bd1
Jye Nicolson <jnicolson@seek.com.au>
2017-08-14 16:54:20 +1000

    chore(tests): add test infrastructure

---
+++ test/index.test.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+var clean = require('rimraf');
+var getSubDirsSync = require('./utils/get-sub-dirs-sync');
+var dirCompare = require('dir-compare');
+var spawn = require('cross-spawn');
+
+var testCases = getSubDirsSync(__dirname + '/test-cases');
+
+describe('Test cases', function() {
+  testCases.forEach(function(testCase) {
+    describe(testCase, function() {
+      var testCaseRoot = __dirname + '/test-cases/' + testCase;
+
+      beforeEach(function(done) {
+        clean(testCaseRoot + '/actual-output', done);
+      });
+
+      it('should build as expected', function() {
+        spawn.sync('node', [__dirname + '/../scripts/build'], {
+          stdio: 'inherit',
+          cwd: testCaseRoot
+        });
+        expect(
+          dirCompare.compareSync(
+            testCaseRoot + '/actual-output',
+            testCaseRoot + '/expected-output',
+            { compareContent: true }
+          ).same
+        ).toBe(true);
+      });
+    });
+  });
+});

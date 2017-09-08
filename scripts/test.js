a62d28410973bd6185c8af24939fc0c497c18dfa
Jye Nicolson <jnicolson@seek.com.au>
2017-09-08 12:30:45 +1000

    feat(tests): allow dangerouslyOverrideJestConfig

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,14 @@
 const jest = require('jest');
-const jestConfig = require('../config/jest/jest.config');
-
+const generateJestConfig = require('../config/jest/jest.config')
+  .generateJestConfig;
 const argv = process.argv.slice(2);
+const builds = require('../config/builds');
+
+const overrideConfig =
+  Array.isArray(builds) && builds.length && builds[0].jestOverrideConfig
+    ? builds[0].jestOverrideConfig
+    : {};
+const jestConfig = generateJestConfig(overrideConfig);
 
 if (!process.env.CI && argv.indexOf('--coverage') < 0) {
   argv.push('--watch');

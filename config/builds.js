b9d3659934a899e4f922840037d91896c91e59cf
Jye Nicolson <jnicolson@seek.com.au>
2017-09-12 13:41:35 +1000

    feat(tests): allow dangerouslySetJestConfig

--- config/builds.js
+++ config/builds.js
@@ -50,6 +50,7 @@ const builds = buildConfigs
     const port = buildConfig.port || 8080;
     const webpackDecorator =
       buildConfig.dangerouslySetWebpackConfig || defaultDecorator;
+    const jestOverrideConfig = buildConfig.dangerouslySetJestConfig || {};
 
     let renderEntry = path.join(cwd, 'src/render.js');
     let serverEntry = null;
@@ -81,6 +82,7 @@ const builds = buildConfigs
       paths,
       locales,
       webpackDecorator,
+      jestOverrideConfig,
       hosts,
       port
     };

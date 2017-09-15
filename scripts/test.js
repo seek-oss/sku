31d0a3dfb3926f8567929d6844b14c9ff57aeb0e
Jye Nicolson <jnicolson@seek.com.au>
2017-09-15 14:28:52 +1000

    feat(tests): allow dangerouslySetJestConfig

--- scripts/test.js
+++ scripts/test.js
@@ -3,7 +3,11 @@ const baseJestConfig = require('../config/jest/jest.config');
 const argv = process.argv.slice(2);
 const builds = require('../config/builds');
 
-const jestConfig = builds[0].jestDecorator(baseJestConfig);
+//Decorate jest config is not supported for monorepo
+const jestConfig =
+  builds.length === 1
+    ? builds[0].jestDecorator(baseJestConfig)
+    : baseJestConfig;
 
 if (!process.env.CI && argv.indexOf('--coverage') < 0) {
   argv.push('--watch');

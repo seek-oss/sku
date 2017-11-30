9244777f0504db5a0acf75029c23547c82fbe3ec
Jahred Hope <jhope@seek.com.au>
2017-11-30 16:24:21 +1100

    feat(lint): Add support for lint-staged style arguments

--- scripts/format.js
+++ scripts/format.js
@@ -3,9 +3,14 @@ const chalk = require('chalk');
 const prettierWrite = require('../lib/runPrettier').write;
 const prettierConfig = require('../config/prettier/prettierConfig');
 
+const args = process.argv.slice(2);
+
 console.log(chalk.cyan('Formatting source code with Prettier'));
 
-prettierWrite(prettierConfig)
+const defaultFilePattern = 'src/**/*.js';
+const filePattern = args.length === 0 ? defaultFilePattern : args;
+
+prettierWrite(filePattern, prettierConfig)
   .then(() => {
     console.log(chalk.cyan('Prettier format complete'));
   })
--- scripts/lint.js
+++ scripts/lint.js
@@ -6,6 +6,8 @@ const builds = require('../config/builds');
 const prettierCheck = require('../lib/runPrettier').check;
 const prettierConfig = require('../config/prettier/prettierConfig');
 
+const args = process.argv.slice(2);
+
 // Decorate eslint config is not supported for monorepo
 const eslintConfig =
   builds.length === 1
@@ -17,8 +19,10 @@ const cli = new EslintCLI({
   useEslintrc: false
 });
 
+const pathsToCheck = args.length === 0 ? ['src'] : args;
+
 const formatter = cli.getFormatter();
-const report = cli.executeOnFiles(['src']);
+const report = cli.executeOnFiles(pathsToCheck);
 
 console.log(chalk.cyan('Linting'));
 console.log(formatter(report.results));
@@ -28,7 +32,11 @@ if (report.errorCount > 0) {
 }
 
 console.log(chalk.cyan('Checking Prettier format rules'));
-prettierCheck(prettierConfig)
+
+const defaultFilePattern = 'src/**/*.js';
+const filePattern = args.length === 0 ? defaultFilePattern : args;
+
+prettierCheck(filePattern, prettierConfig)
   .then(() => {
     console.log(chalk.cyan('Prettier format rules passed'));
   })

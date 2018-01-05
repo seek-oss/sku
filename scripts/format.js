b89d344ba26d2bfda97b7072da385023a0f50ed6
Jahred Hope <jahredhope@gmail.com>
2018-01-05 16:08:05 +1100

    feat(lint): Add support for lint-staged style arguments (#95)
    * feat(lint): Add support for lint-staged style arguments
    
    * refactor(lint): Hoist default paths to common directory

--- scripts/format.js
+++ scripts/format.js
@@ -3,9 +3,15 @@ const chalk = require('chalk');
 const prettierWrite = require('../lib/runPrettier').write;
 const prettierConfig = require('../config/prettier/prettierConfig');
 
+const defaultPath = require('../config/prettier/defaultPath');
+
+const args = process.argv.slice(2);
+
 console.log(chalk.cyan('Formatting source code with Prettier'));
 
-prettierWrite(prettierConfig)
+const filePattern = args.length === 0 ? defaultPath : args;
+
+prettierWrite(filePattern, prettierConfig)
   .then(() => {
     console.log(chalk.cyan('Prettier format complete'));
   })

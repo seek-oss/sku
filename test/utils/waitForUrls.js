abcfed313beb343e50f806e20c50683ef38b76e8
Mike Fisher <mike@mbfisher.com>
2018-08-10 11:01:12 +1000

    feat(test): Add context error for waitForUrls

--- test/utils/waitForUrls.js
+++ test/utils/waitForUrls.js
@@ -2,9 +2,19 @@ const { promisify } = require('es6-promisify');
 const waitOnAsync = promisify(require('wait-on'));
 
 module.exports = async (...urls) => {
-  return await waitOnAsync({
-    resources: urls.map(url => url.replace(/^http/, 'http-get')),
-    headers: { accept: 'text/html, application/javascript' },
-    timeout: 35000
-  });
+  const timeout = 30000;
+
+  try {
+    return await waitOnAsync({
+      resources: urls.map(url => url.replace(/^http/, 'http-get')),
+      headers: { accept: 'text/html, application/javascript' },
+      timeout
+    });
+  } catch (error) {
+    if (error.message === 'Timeout') {
+      throw new Error(`waitForUrls waited ${timeout}ms for ${urls.join(', ')}`);
+    }
+
+    throw error;
+  }
 };

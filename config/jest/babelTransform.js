aec2213000c32fe2443cf54ccbdbc50db6160696
Mike Fisher <mfisher@seek.com.au>
2018-08-27 15:00:45 +1000

    feat(babel): Use babelDecorator in Jest config

--- config/jest/babelTransform.js
+++ config/jest/babelTransform.js
@@ -1,4 +1,9 @@
 const babelJest = require('babel-jest');
 const babelConfig = require('../babel/babelConfig');
+const builds = require('../builds');
 
-module.exports = babelJest.createTransformer(babelConfig({ target: 'node' }));
+const decorator = builds[0].babelDecorator || (config => config);
+
+module.exports = babelJest.createTransformer(
+  decorator(babelConfig({ target: 'node' }))
+);

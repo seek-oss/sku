fa2cc422e4177d76283c3b270e779eaa65f42ceb
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2018-09-17 10:28:38 +1000

    fix: Fix seek-style-guide mocks

--- config/jest/jestConfig.js
+++ config/jest/jestConfig.js
@@ -4,13 +4,20 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|svg)$': require.resolve(
       './fileMock'
-    )
+    ),
+
+    // Mock out all seek-style-guide components with strings,
+    // using a proxy object that echoes back the import name,
+    // e.g. `import { Text } from 'seek-style-guide/react'` resolves
+    // to the string 'Text'. This way, snapshot tests won't break when
+    // seek-style-guide gets updated, which happens regularly. There's
+    // still room for debate about whether this is a good idea or not...
+    'seek-style-guide/react': require.resolve('identity-obj-proxy')
   },
   transform: {
     '^.+\\.css\\.js$': require.resolve('./cssJsTransform.js'),
-    '(seek-style-guide/react|.(css|less)$)': require.resolve(
-      './cssModulesTransform.js'
-    ),
+    '\\.(css|less)$': require.resolve('./cssModulesTransform.js'),
+
     // Match any `.js` file that isn't a `.css.js` file.
     // We do this by asserting the 4 characters before `.js` aren't `.css`
     // or that it has fewer than 4 characters (e.g. `foo.js`)
---
+++ test/test-cases/seek-style-guide/app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleGuideProvider, PageBlock, Section, Text } from 'seek-style-guide/react';
+
+export default () => (
+  <StyleGuideProvider>
+    <PageBlock>
+      <Section>
+        <Text>Hello world!</Text>
+      </Section>
+    </PageBlock>
+  </StyleGuideProvider>
+);

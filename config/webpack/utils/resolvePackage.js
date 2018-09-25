175d90f6a8e1c5c44a2ae704c3e76337b033077e
Mike Fisher <mike@mbfisher.com>
2018-09-25 13:46:03 +1000

    docs: Improve resolvePackage docs

--- config/webpack/utils/resolvePackage.js
+++ config/webpack/utils/resolvePackage.js
@@ -3,36 +3,26 @@ const memoize = require('memoizee');
 const chalk = require('chalk');
 const debug = require('debug')('sku:resolvePackage');
 
-const getProjectDependencies = readFileSync => {
-  let pkg;
-
-  try {
-    pkg = JSON.parse(readFileSync(`${process.cwd()}/package.json`).toString());
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      pkg = {};
-    } else {
-      throw error;
-    }
-  }
-
-  return {
-    ...(pkg.dependencies || {}),
-    ...(pkg.devDependencies || {})
-  };
-};
-
 /**
- * Resolve a package name to an absolute path.
- * e.g. my-package -> /Users/me/code/my-project/node_modules/my-package
+ * Create a `resolvePackage` function.
  *
- * Throws if a package is listed in the project's dependencies and cannot be resolved.
+ * This wrapper let's us inject fs and require dependencies for testing.
+ *
+ * @param {Object} fs - Node's fs module
+ * @param {Function} resolve - Node's require.resolve
  */
-const createPackageResolver = (fs, resolve) =>
-  memoize(function resolvePackage(packageName) {
+const createPackageResolver = (fs, resolve) => {
+  /**
+   * Resolve a package name to an absolute path.
+   * e.g. my-package -> /Users/me/code/my-project/node_modules/my-package
+   *
+   * Throws if a package is listed in the project's dependencies and cannot be resolved.
+   */
+  function resolvePackage(packageName) {
     const cwd = process.cwd();
 
-    // First, look for a local copy of the package. If it exists, we're done.
+    // First, look for a copy of the package in the project's node_modules.
+    // If it exists, we're done.
     const localPackage = path.join(cwd, 'node_modules', packageName);
 
     if (fs.existsSync(localPackage)) {
@@ -40,9 +30,11 @@ const createPackageResolver = (fs, resolve) =>
       return localPackage;
     }
 
-    // If there's no local package, check to see if the project has it as a dependency. If it does
-    // we'll try and track it down, if not we'll return a naive local path. This prevents webpack
-    // from throwing invalid config errors.
+    // If there's no local package, check to see if it's listed as a project dependency. If it is
+    // we'll try and track it down, if not we'll return a naive local path.
+    // This branch handles the scenario where we're trying to resolve seek-style-guide because
+    // it's on the default list of compilePackages, but it's not actually being used in the project.
+    // Returning a naive path prevents webpack from throwing config schema errors.
     const dependencies = getProjectDependencies(fs.readFileSync);
 
     if (!dependencies[packageName]) {
@@ -50,10 +42,12 @@ const createPackageResolver = (fs, resolve) =>
       return localPackage;
     }
 
-    // If we don't find a local copy we use Node's require algorithm to find one.
+    // We haven't found a copy of the package in the project node_modules directory, and we know
+    // it's listed as a project dependency, so we use Node's require algorithm to track it down.
     // This branch is used when sku or seek-style-guide have been `npm link`ed into a project,
     // and also by the seek-style-guide.test.js sku test.
     // Doing this can produce hard-to-debug build issues, so we log a warning.
+    // `resolve` will throw if the package name can't be resolved.
     const resolvedPackage = path.dirname(
       resolve(`${packageName}/package.json`)
     );
@@ -70,7 +64,29 @@ const createPackageResolver = (fs, resolve) =>
 
     debug(`Resolved ${packageName} to ${resolvedPackage}`);
     return resolvedPackage;
-  });
+  }
+
+  return memoize(resolvePackage);
+};
+
+function getProjectDependencies(readFileSync) {
+  let pkg;
+
+  try {
+    pkg = JSON.parse(readFileSync(`${process.cwd()}/package.json`).toString());
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      pkg = {};
+    } else {
+      throw error;
+    }
+  }
+
+  return {
+    ...(pkg.dependencies || {}),
+    ...(pkg.devDependencies || {})
+  };
+}
 
 module.exports = {
   resolvePackage: createPackageResolver(require('fs'), require.resolve),

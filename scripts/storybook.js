7017f5d86d1cb7ebd8682f9189737ceea27b23ad
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2018-10-09 11:02:29 +1100

    refactor: Clean up storybook script

--- scripts/storybook.js
+++ scripts/storybook.js
@@ -1,10 +1,9 @@
 const path = require('path');
 const gracefulSpawn = require('../lib/gracefulSpawn');
+const builds = require('../config/builds');
+const { storybookPort } = builds[0];
 const startStorybookPath = require.resolve('@storybook/react/bin/index.js');
 const configDir = path.resolve(__dirname, '..', 'config', 'storybook');
-const builds = require('../config/builds');
-
-const { storybookPort } = builds[0];
 
 const storybookProcess = gracefulSpawn(
   startStorybookPath,

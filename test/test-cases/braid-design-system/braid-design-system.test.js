d3982372102216408904017eb61b40fa27e298bf
Jye Nicolson <jyenicolson@gmail.com>
2018-10-18 11:15:55 +1100

    fix(deps): Upgrade less to v3
    fix(deps): Upgrade less to v3

--- test/test-cases/braid-design-system/braid-design-system.test.js
+++ test/test-cases/braid-design-system/braid-design-system.test.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const { promisify } = require('es6-promisify');
 const rimrafAsync = promisify(require('rimraf'));
-const fs = require('fs').promises;
+const fs = require('fs-extra');
 const dirContentsToObject = require('../../utils/dirContentsToObject');
 const runSkuScriptInDir = require('../../utils/runSkuScriptInDir');
 const appDir = path.resolve(__dirname, 'app');

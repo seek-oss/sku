12f1ae3499afd803295bdc742d08174dd8f402c4
Matt Jones <mattjones701@gmail.com>
2018-10-22 16:17:44 +1100

    chore: Remove unused story in test case

--- test/test-cases/typescript-css-modules/app/src/App.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import App from './App';
-
-// Typically would be importing from 'sku/storybook'
-import { storiesOf } from '../../../../../storybook';
-
-storiesOf('App', module).add('Default', () => <App />);

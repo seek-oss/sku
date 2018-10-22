dcda62e35b764df2a8308af7010f80f8ae4b3a4e
michaeltaranto <michael.taranto@gmail.com>
2018-10-22 14:14:45 +1100

    build(gitignore): Control ignoring of sku-created config files

---
+++ scripts/configure.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+const path = require('path');
+const ensureGitignore = require('ensure-gitignore');
+const uniq = require('lodash/uniq');
+const builds = require('../config/builds');
+
+const outputDirs = uniq([
+  ...builds.map(
+    ({ paths }) =>
+      `${paths.dist.replace(`${process.cwd()}${path.sep}`, '')}${path.sep}`
+  )
+]);
+
+ensureGitignore({
+  comment: 'managed by sku',
+  patterns: [...outputDirs, `report${path.sep}`]
+});

a11f151bff73d893ed240320df40ef7099e1cef5
Matt Jones <mattjones701@gmail.com>
2018-11-16 10:42:03 +1100

    test: Add hot reload to SSR test-case

--- test/test-cases/ssr-hello-world/src/client.js
+++ test/test-cases/ssr-hello-world/src/client.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
 import App from './App';
 
-hydrate(<App />, document.getElementById('app'));
+const render = Component => {
+  hydrate(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('app')
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App);
+  });
+}

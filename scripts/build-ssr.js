d677c2e776dd051a06448db2b0e332bb11ff3eb0
Matt Jones <mattjones701@gmail.com>
2018-11-16 12:13:56 +1100

    refactor: Lib scripts

--- scripts/build-ssr.js
+++ scripts/build-ssr.js
@@ -2,28 +2,20 @@
 process.env.NODE_ENV = 'production';
 
 const webpack = require('webpack');
-const { promisify } = require('util');
-const rimraf = promisify(require('rimraf'));
 
 const { run } = require('../lib/runWebpack');
 const copyPublicFiles = require('../lib/copyPublicFiles');
+const cleanTargetFolder = require('../lib/cleanTargetFolder');
 const [
   clientConfig,
   serverConfig
 ] = require('../config/webpack/webpack.config.ssr');
-const { paths } = require('../context');
-
-const cleanTargetFolder = () => rimraf(`${paths.target}/*`);
 
 (async () => {
   try {
     await cleanTargetFolder();
     await run(webpack(clientConfig));
-    console.log('Client webpack done');
-
     await run(webpack(serverConfig));
-    console.log('Server webpack done');
-
     await copyPublicFiles();
 
     console.log('Sku build complete!');
--- scripts/build.js
+++ scripts/build.js
@@ -6,46 +6,23 @@ const { promisify } = require('util');
 const rimraf = promisify(require('rimraf'));
 
 const copyPublicFiles = require('../lib/copyPublicFiles');
+const cleanTargetFolder = require('../lib/cleanTargetFolder');
+
+const { run } = require('../lib/runWebpack');
 const webpackCompiler = require('../config/webpack/webpack.config');
 const { paths } = require('../context');
 
-const compile = () => {
-  return new Promise((resolve, reject) => {
-    webpackCompiler.run((error, stats) => {
-      console.log(
-        stats.toString({
-          chunks: false, // Makes the build much quieter
-          children: false,
-          colors: true
-        })
-      );
-
-      if (error || stats.hasErrors()) {
-        // Webpack has already printed the errors, so we just need to stop execution.
-        reject();
-      }
-
-      const info = stats.toJson();
-
-      if (stats.hasWarnings()) {
-        info.warnings.forEach(console.warn);
-      }
-
-      resolve();
-    });
-  });
-};
-
-const cleanDistFolders = () => rimraf(`${paths.target}/*`);
-
 const cleanRenderJs = () => rimraf(path.join(paths.target, 'render.js'));
 
-cleanDistFolders()
-  .then(compile)
-  .then(cleanRenderJs)
-  .then(copyPublicFiles)
-  .then(() => console.log('Sku build complete!'))
-  .catch(error => {
+(async () => {
+  try {
+    await cleanTargetFolder();
+    await run(webpackCompiler);
+    await cleanRenderJs();
+    await copyPublicFiles();
+    console.log('Sku build complete!');
+  } catch (error) {
     console.error(error);
     process.exit(1);
-  });
+  }
+})();

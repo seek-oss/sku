9fc55ae59439ccbcabd6215f7c7cb2da9a1040be
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2018-11-23 13:34:17 +1100

    feat: Generate ESLint config files (#214)

--- test/test-cases/typescript-css-modules/app/sku.config.js
+++ test/test-cases/typescript-css-modules/app/sku.config.js
@@ -3,5 +3,13 @@ module.exports = {
     client: 'src/client.tsx',
     render: 'src/render.tsx',
     server: 'src/server.tsx'
+  },
+  // Use 'dangerouslySetESLintConfig' so we can
+  // check that overrides are written to disk.
+  dangerouslySetESLintConfig: config => {
+    config.rules = config.rules || {};
+    config.rules['no-console'] = 0;
+
+    return config;
   }
 };

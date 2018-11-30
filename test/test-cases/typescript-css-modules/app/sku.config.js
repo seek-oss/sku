6ef3a8b714250a2e8b3957c689b9aba7d6b34fe8
michaeltaranto <michael.taranto@gmail.com>
2018-11-30 12:18:38 +1100

    Revert "chore: Merge master to v7-beta (#228)"
    This reverts commit c4d06277b239d0a8c03d8a5aa7ef3cc929c91472.

--- test/test-cases/typescript-css-modules/app/sku.config.js
+++ test/test-cases/typescript-css-modules/app/sku.config.js
@@ -4,5 +4,13 @@ module.exports = {
   serverEntry: 'src/server.tsx',
   port: 8204,
   serverPort: 8010,
-  publicPath: 'http://localhost:4003'
+  publicPath: 'http://localhost:4003',
+  // Use 'dangerouslySetESLintConfig' so we can
+  // check that overrides are written to disk.
+  dangerouslySetESLintConfig: config => {
+    config.rules = config.rules || {};
+    config.rules['no-console'] = 0;
+
+    return config;
+  }
 };

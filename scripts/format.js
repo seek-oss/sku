19b0bb8d77090313ee423b2253b38f750d61353d
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2018-12-06 11:24:40 +1100

    feat(format): Run ESLint fix during format (#236)

--- scripts/format.js
+++ scripts/format.js
@@ -1,6 +1,19 @@
+const chalk = require('chalk');
+const EslintCLI = require('eslint').CLIEngine;
+const eslintConfig = require('../config/eslint/eslintConfig');
 const prettierWrite = require('../lib/runPrettier').write;
 const args = require('../config/args').argv;
 
-const pathsToCheck = args.length === 0 ? [] : args;
+console.log(chalk.cyan('Fixing code with ESLint'));
+const eslintCli = new EslintCLI({
+  baseConfig: eslintConfig,
+  useEslintrc: false,
+  fix: true
+});
+const eslintPathsToCheck = args.length === 0 ? ['.'] : args;
+const eslintReport = eslintCli.executeOnFiles(eslintPathsToCheck);
+EslintCLI.outputFixes(eslintReport);
+console.log(chalk.cyan('ESLint fix complete'));
 
-prettierWrite(pathsToCheck);
+const prettierPathsToCheck = args.length === 0 ? [] : args;
+prettierWrite(prettierPathsToCheck);

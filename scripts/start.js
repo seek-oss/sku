1d9072ff5ba73e680e5f7df4fb54c0d1f300b50b
matt <mattjones701@gmail.com>
2019-01-09 07:08:47 +1100

    feat(start): Assign free port is requested one is unavailable

--- scripts/start.js
+++ scripts/start.js
@@ -2,6 +2,8 @@ process.env.NODE_ENV = 'development';
 
 const opn = require('opn');
 const WebpackDevServer = require('webpack-dev-server');
+const getPort = require('get-port');
+const { yellow, blue, underline, bold } = require('chalk');
 
 const dynamicRouteMiddleware = require('../lib/dynamicRouteMiddleware');
 const { hosts, port, initialPath, paths, routes } = require('../context');
@@ -28,19 +30,34 @@ const devServer = new WebpackDevServer(webpackCompiler, {
   }
 });
 
-devServer.listen(port.client, '0.0.0.0', err => {
-  if (err) {
-    console.log(err);
-    return;
+(async () => {
+  const localhost = '0.0.0.0';
+  const availablePort = await getPort({ port: port.client, host: localhost });
+
+  if (availablePort !== port.client) {
+    console.log(
+      yellow(
+        `Warning: Requested port ${bold(
+          port.client
+        )} is unavailable. Falling back to ${bold(availablePort)}.`
+      )
+    );
   }
 
-  const url = `http://${hosts[0]}:${port.client}${initialPath}`;
+  devServer.listen(availablePort, localhost, err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
 
-  console.log();
-  console.log(`Starting the development server on ${url}...`);
-  console.log();
+    const url = `http://${hosts[0]}:${availablePort}${initialPath}`;
 
-  if (process.env.OPEN_TAB !== 'false') {
-    opn(url);
-  }
-});
+    console.log();
+    console.log(blue(`Starting the development server on ${underline(url)}`));
+    console.log();
+
+    if (process.env.OPEN_TAB !== 'false') {
+      opn(url);
+    }
+  });
+})();

3de185b1656cb5fa1a83c9d451dd6f577d2eb47e
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2019-01-22 09:23:01 +1100

    test: Add test case for TypeScript file in ‘setupTests’

--- test/test-cases/typescript-css-modules/app/sku.config.js
+++ test/test-cases/typescript-css-modules/app/sku.config.js
@@ -4,5 +4,6 @@ module.exports = {
   serverEntry: 'src/server.tsx',
   port: 8204,
   serverPort: 8010,
-  publicPath: 'http://localhost:4003'
+  publicPath: 'http://localhost:4003',
+  setupTests: 'src/setupTests.ts'
 };
---
+++ test/test-cases/typescript-css-modules/app/src/setupTests.ts
@@ -0,0 +1,1 @@
+Object.defineProperty(global, 'SETUP_TESTS_SCRIPT_RAN', { value: true });

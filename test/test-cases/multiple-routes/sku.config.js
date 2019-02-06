af2910301b37d3dd08074e167691ac88a5be12e9
Matt Jones <mattjones701@gmail.com>
2019-02-06 11:13:11 +1100

    fix(config): Offer simpler route config option

--- test/test-cases/multiple-routes/sku.config.js
+++ test/test-cases/multiple-routes/sku.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  routes: [
-    { name: 'home', route: '/' },
-    { name: 'details', route: '/details/:id' }
-  ],
+  routes: ['/', '/details/:id'],
   sites: ['au', 'nz'],
   environments: ['production'],
   port: 8202,

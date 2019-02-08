a7029ed68c0951282079d7b2f930b657a8ae0323
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2019-02-08 11:24:06 +1100

    Update @loadable/component.ts
    Co-Authored-By: mattsjones <mattjones701@gmail.com>

--- @loadable/component.ts
+++ @loadable/component.ts
@@ -1,3 +1,3 @@
-// This is provided so consumers can import `sku/loadable/component`,
+// This is provided so consumers can import `sku/@loadable/component`,
 // since they don't depend on `@loadable/component` directly.
 export * from '@loadable/component';

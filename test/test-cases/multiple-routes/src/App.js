4b4cde59693daab7aef967d480529c8b649fe7eb
mattsjones <mattjones701@gmail.com>
2019-02-08 14:30:45 +1100

    fix: Move loadable component to a normal dependency (#298)

--- test/test-cases/multiple-routes/src/App.js
+++ test/test-cases/multiple-routes/src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-no-bind */
 import React, { Fragment } from 'react';
 import { Route } from 'react-router-dom';
-import loadable from '@loadable/component';
+import loadable from '../../../../@loadable/component';
 
 const Home = loadable(() => import('./handlers/Home'));
 const Details = loadable(() => import('./handlers/Details'));

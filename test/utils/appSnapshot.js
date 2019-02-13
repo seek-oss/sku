6baff0dd9c404091e8d7092d3f6d01f4b195d8a9
Matt Jones <mattjones701@gmail.com>
2019-02-13 11:47:16 +1100

    diff working kinda

--- test/utils/appSnapshot.js
+++ test/utils/appSnapshot.js
@@ -1,5 +1,61 @@
 const LineDiff = require('line-diff');
 
+const getLineDiff = (html1, html2) => {
+  const diffs = [];
+  const lineBuffer = 3;
+
+  const changes = new LineDiff(html1, html2).changes;
+
+  let bufferCount = lineBuffer;
+  let currentDiff = null;
+
+  changes.forEach((change, index) => {
+    if (currentDiff && !change.modified) {
+      bufferCount--;
+      currentDiff.push(change);
+    }
+
+    if (change.modified) {
+      bufferCount = lineBuffer;
+    }
+
+    if (!currentDiff && change.modified) {
+      const bufferStart = index - bufferCount > 0 ? index - bufferCount : 0;
+      const previousBuffer = [...changes].splice(bufferStart, bufferCount);
+      currentDiff = [...previousBuffer, change];
+    }
+
+    if (bufferCount === 0) {
+      diffs.push(currentDiff);
+      bufferCount = lineBuffer;
+      currentDiff = null;
+    }
+  });
+
+  return diffs
+    .map(diff =>
+      diff
+        .map(change => {
+          if (change.modified) {
+            const [indent] = change._[0].match(/^(\s*)/);
+
+            const stringDiff = [
+              `${indent}-${change._[0].trim()}`,
+              `${indent}+${change._[1].trim()}`
+            ];
+
+            console.log(stringDiff);
+
+            return stringDiff.join('\n');
+          }
+
+          return change._[0];
+        })
+        .join('\n')
+    )
+    .join('===============================================\n');
+};
+
 const appSnapshotSerializer = {
   print: (
     { sourceHtml, clientRenderContent, warnings, errors },
@@ -8,17 +64,14 @@ const appSnapshotSerializer = {
     const serializedSouceHtml = serializer(sourceHtml);
     const serializedClientRenderContent = serializer(clientRenderContent);
 
-    const htmlDiff = new LineDiff(
+    const htmlDiff = getLineDiff(
       serializedSouceHtml,
       serializedClientRenderContent
     );
 
-    console.log(JSON.stringify(htmlDiff.changes, null, 2));
-
     const snapshotItems = [
       `SOURCE HTML: ${serializedSouceHtml}`,
-      `WARNINGS: ${serializer(warnings)}`,
-      `POST HYDRATE DIFF: ${htmlDiff.toString()}`,
+      `POST HYDRATE DIFFS: \n${htmlDiff}`,
       `WARNINGS: ${serializer(warnings)}`,
       `ERRORS: ${serializer(errors)}`
     ];

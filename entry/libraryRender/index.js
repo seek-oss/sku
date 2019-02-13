73b6cdd1a87aa15fbcf74b4a05ecb5fa9e8aa35a
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2019-02-13 15:13:30 +1100

    fix(library): Fix development HTML in library mode

---
+++ entry/libraryRender/index.js
@@ -0,0 +1,14 @@
+import render from '__sku_alias__renderEntry';
+
+const libraryName = SKU_LIBRARY_NAME;
+const publicPath = __SKU_PUBLIC_PATH__;
+
+export default async renderParams => {
+  const renderContext = { ...renderParams, libraryName };
+
+  return await render.renderDocument({
+    ...renderContext,
+    headTags: `<link rel="stylesheet" type="text/css" href="${publicPath}${libraryName}.css">`,
+    bodyTags: `<script src="${publicPath}${libraryName}.js"></script>`
+  });
+};

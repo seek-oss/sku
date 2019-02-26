6fc988c15f2bd45caee7081f2d7a2a28d48567c7
Matt Jones <mattjones701@gmail.com>
2019-02-26 13:50:14 +1100

    fix(babel): Add source type option to babel config for module inference
    This fixes an issue where babel can get confused about what type of file it is parsing
    (module/scripts). This is specifically a problem in sku for compile packages. See the related babel
    issue here https://github.com/babel/babel/issues/9238.

--- config/babel/babelConfig.js
+++ config/babel/babelConfig.js
@@ -58,6 +58,7 @@ module.exports = ({ target, lang = 'js' }) => {
 
   return {
     babelrc: false,
+    sourceType: 'unambiguous',
     presets: [
       languagePreset,
       [require.resolve('@babel/preset-env'), envPresetOptions],

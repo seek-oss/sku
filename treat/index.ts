98cd3358765faba9062863b1686cf15207b8ae94
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2019-04-17 14:40:24 +1000

    feat(treat): Add `sku/treat` re-export so consumers can import treat

---
+++ treat/index.ts
@@ -0,0 +1,26 @@
+// This is provided so consumers can import `sku/treat`,
+// since they don't depend on `treat` directly.
+
+// We can't re-export directly because eslint-plugin-import doesn't understand :(
+
+import {
+  createTheme,
+  style,
+  css,
+  TreatProvider,
+  useClassNames,
+  useStyles,
+  resolveClassNames,
+  resolveStyles,
+} from 'treat';
+
+export {
+  createTheme,
+  style,
+  css,
+  TreatProvider,
+  useClassNames,
+  useStyles,
+  resolveClassNames,
+  resolveStyles,
+};

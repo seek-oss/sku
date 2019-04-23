11a9ccf1534fc77a94d66975fa0ab78d2d8c533f
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2019-04-23 14:02:55 +1000

    fix: Tweak generated Treat class names

--- config/webpack/plugins/createTreatPlugin.js
+++ config/webpack/plugins/createTreatPlugin.js
@@ -3,12 +3,12 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = ({ target, isStartScript }) => {
   const localIdentName = isStartScript
-    ? '[name]-[local]_[hash:base64:5]'
+    ? 'BRAID__[name]-[local]_[hash:base64:5]'
     : '[hash:base64:5]';
 
   const themeIdentName = isStartScript
-    ? '-[folder]_[hash:base64:5]'
-    : '[hash:base64:2]';
+    ? '-[folder]_[hash:base64:3]'
+    : '[hash:base64:3]';
 
   return new TreatPlugin({
     test: /braid-design-system\/(?!node_modules).+\.treat\.(?:js|ts)/,

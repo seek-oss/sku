bf3bdcf28d8682a7c4a63b2c875973c3d7376d50
Shawn <shdsouza@seek.com.au>
2019-11-28 17:30:39 +1100

    wip

--- config/eslint/eslintConfig.js
+++ config/eslint/eslintConfig.js
@@ -1,70 +1,25 @@
 const ts = {
   files: ['**/*.ts', '**/*.tsx'],
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks',
+  ],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
   rules: {
-    'no-empty-function': 'error',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
     'react/jsx-filename-extension': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
   },
-  globals: {
-    ENV: 'off',
-  },
 };
-
-// const working = {
-//   env: { browser: true, es6: true, node: true },
-//   extends: ['eslint:recommended', 'plugin:react/recommended'],
-//   globals: { Atomics: 'readonly', SharedArrayBuffer: 'readonly' },
-//   parser: 'babel-eslint',
-//   parserOptions: {
-//     ecmaFeatures: { jsx: true },
-//     ecmaVersion: 2018,
-//     sourceType: 'module',
-//   },
-//   plugins: ['react'],
-//   rules: {
-//     indent: ['error', 2, { SwitchCase: 1 }],
-//     'linebreak-style': ['error', 'unix'],
-//     quotes: ['error', 'single'],
-//     'comma-dangle': ['error', 'always-multiline'],
-//     semi: ['error', 'always'],
-//   },
-//   settings: { react: { version: 'detect' } },
-//   overrides: [
-//     {
-//       files: ['**/*.ts', '**/*.tsx'],
-//       env: { browser: true, es6: true, node: true },
-//       extends: [
-//         'eslint:recommended',
-//         'plugin:react/recommended',
-//         'plugin:@typescript-eslint/eslint-recommended',
-//         'plugin:@typescript-eslint/recommended',
-//       ],
-//       globals: { Atomics: 'readonly', SharedArrayBuffer: 'readonly' },
-//       parser: '@typescript-eslint/parser',
-//       parserOptions: {
-//         ecmaFeatures: { jsx: true },
-//         ecmaVersion: 2018,
-//         sourceType: 'module',
-//         project: './tsconfig.json',
-//       },
-//       plugins: ['react', '@typescript-eslint'],
-//       rules: {
-//         indent: ['error', 2, { SwitchCase: 1 }],
-//         'linebreak-style': ['error', 'unix'],
-//         quotes: ['error', 'single'],
-//         'comma-dangle': ['error', 'always-multiline'],
-//         '@typescript-eslint/no-explicit-any': 0,
-//       },
-//       settings: { react: { version: 'detect' } },
-//     },
-//   ],
-// };
 
 const { eslintDecorator } = require('../../context');
 

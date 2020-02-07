da3c06e3612c62488b78db9a080fe6e77a827af2
Matt Jones <mattjones701@gmail.com>
2020-02-07 14:33:20 +1100

    test: Add tests for sku-webpack-plugin

---
+++ test/test-cases/sku-webpack-plugin/app/main.js
@@ -0,0 +1,19 @@
+import 'braid-design-system/reset';
+import theme from 'braid-design-system/themes/seekAnz';
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import { BraidProvider, Card, Text } from 'braid-design-system';
+
+const App = () => (
+  <BraidProvider theme={theme}>
+    <Card>
+      <Text size="large">Is it working?</Text>
+    </Card>
+  </BraidProvider>
+);
+
+ReactDom.render(
+  <App />,
+  document.body.appendChild(document.createElement('div')),
+);

d88f6dffb381b5b5e36f6279b24f7bb8df1bada6
Matt Jones <mattjones701@gmail.com>
2020-04-17 11:05:27 +1000

    Move away from optional dependencies

--- telemetry/provider.js
+++ telemetry/provider.js
@@ -1,3 +1,5 @@
+const banner = require('../lib/banner');
+
 function noop() {}
 
 let provider = {
@@ -8,12 +10,16 @@ let provider = {
 };
 
 try {
-  if (process.env.TELEMETRY !== 'false') {
+  if (process.env.SKU_TELEMETRY !== 'false') {
     // eslint-disable-next-line import/no-unresolved
     provider = require('@seek/sku-telemetry').default({});
   }
 } catch (e) {
-  // No telemetry available
+  banner('warning', '@seek/sku-telemetry not installed', [
+    'To help us improve sku, please install our private telemetry package that gives us insights on usage, errors and performance.',
+    'yarn add --dev @seek/sku-telemetry',
+    'Non SEEK based usage can disable this message with `SKU_TELEMETRY=false`',
+  ]);
 }
 
 module.exports = provider;

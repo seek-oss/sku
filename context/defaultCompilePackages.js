1d3002df9bc6f6c18cff56b37a19c60a3814f179
Matt Jones <mattjones701@gmail.com>
2020-10-16 12:53:13 +1100

    Automatically detect compile packages with @seek scope

--- context/defaultCompilePackages.js
+++ context/defaultCompilePackages.js
@@ -1,6 +1,27 @@
+const path = require('path');
+const glob = require('fast-glob');
+
+const { cwd } = require('../lib/cwd');
+
+const detectedCompilePackages = glob
+  .sync([`node_modules/@seek/*/package.json`], {
+    cwd: cwd(),
+  })
+  .map((packagePath) => {
+    const packageJson = require(path.join(cwd(), packagePath));
+
+    return {
+      isCompilePackage: Boolean(packageJson.skuCompilePackage),
+      packageName: packageJson.name,
+    };
+  })
+  .filter(({ isCompilePackage }) => isCompilePackage)
+  .map(({ packageName }) => packageName);
+
 module.exports = [
   'sku',
   'seek-style-guide',
   'seek-asia-style-guide',
   'braid-design-system',
+  ...detectedCompilePackages,
 ];

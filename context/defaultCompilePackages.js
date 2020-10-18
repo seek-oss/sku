5b6e1caf8e1cd98282aba0954f39898abff6a3d2
Matt Jones <mattjones701@gmail.com>
2020-10-19 09:47:55 +1100

    Improved error handling

--- context/defaultCompilePackages.js
+++ context/defaultCompilePackages.js
@@ -1,22 +1,32 @@
 const path = require('path');
+const chalk = require('chalk');
 const glob = require('fast-glob');
 
 const { cwd } = require('../lib/cwd');
 
-const detectedCompilePackages = glob
-  .sync([`node_modules/@seek/*/package.json`], {
-    cwd: cwd(),
-  })
-  .map((packagePath) => {
-    const packageJson = require(path.join(cwd(), packagePath));
+let detectedCompilePackages = [];
 
-    return {
-      isCompilePackage: Boolean(packageJson.skuCompilePackage),
-      packageName: packageJson.name,
-    };
-  })
-  .filter(({ isCompilePackage }) => isCompilePackage)
-  .map(({ packageName }) => packageName);
+try {
+  detectedCompilePackages = glob
+    .sync([`node_modules/@seek/*/package.json`], {
+      cwd: cwd(),
+    })
+    .map((packagePath) => {
+      const packageJson = require(path.join(cwd(), packagePath));
+
+      return {
+        isCompilePackage: Boolean(packageJson.skuCompilePackage),
+        packageName: packageJson.name,
+      };
+    })
+    .filter(({ isCompilePackage }) => isCompilePackage)
+    .map(({ packageName }) => packageName);
+} catch (e) {
+  console.log(
+    chalk.red`Warning: Failed to detect compile packages. Contact #sku-support.`,
+  );
+  console.error(e);
+}
 
 module.exports = [
   'sku',

923f10f9bcb78a9111555641bd4a40926c4ec33f
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2021-05-31 10:51:24 +1000

    Update Braid test

--- test/test-cases/braid-design-system/app/src/App.test.tsx
+++ test/test-cases/braid-design-system/app/src/App.test.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import seekAnz from 'braid-design-system/themes/seekAnz';
-import { Box, BraidProvider } from 'braid-design-system';
+import { Box, BraidTestProvider } from 'braid-design-system';
 
 describe('braid-design-system', () => {
   test('components', () => {
     expect(
       renderToString(
-        <BraidProvider theme={seekAnz}>
+        <BraidTestProvider themeName="seekAnz">
           <Box paddingTop="large" />
-        </BraidProvider>,
+        </BraidTestProvider>,
       ),
     ).toMatchInlineSnapshot(
-      `"<style type=\\"text/css\\">body{margin:0;padding:0;background:#eee}</style><div><div class=\\"base padding_top_large_makeBraidTheme_treatTheme\\"></div></div>"`,
+      `"<style type=\\"text/css\\">body{margin:0;padding:0;background:#eee}</style><div class=\\"base padding_top_large_makeBraidTheme_treatTheme\\"></div>"`,
     );
   });
 });

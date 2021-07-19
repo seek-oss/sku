869e136c8d4b25ffa97500644e92a845f01e4842
mattcompiles <mattjones701@gmail.com>
2021-07-19 11:39:22 +1000

    Remove flow type support, remove unnecessary babel transforms (#608)

--- config/babel/babelConfig.js
+++ config/babel/babelConfig.js
@@ -12,12 +12,14 @@ module.exports = ({
   const browserEnvOptions = {
     modules: false,
     targets: supportedBrowsers,
+    shippedProposals: true,
   };
 
   const nodeEnvOptions = {
     targets: {
       node: 'current',
     },
+    shippedProposals: true,
   };
 
   const isBrowser = target === 'browser';
@@ -26,14 +28,6 @@ module.exports = ({
 
   const envPresetOptions = isBrowser ? browserEnvOptions : nodeEnvOptions;
   const plugins = [
-    require.resolve('babel-plugin-syntax-dynamic-import'),
-    ...(!isProductionBuild && lang === 'js'
-      ? [require.resolve('babel-plugin-flow-react-proptypes')]
-      : []),
-    require.resolve('@babel/plugin-proposal-class-properties'),
-    require.resolve('@babel/plugin-proposal-object-rest-spread'),
-    require.resolve('@babel/plugin-proposal-optional-chaining'),
-    require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'),
     [
       require.resolve('babel-plugin-module-resolver'),
       {
@@ -55,12 +49,10 @@ module.exports = ({
     ]);
   }
 
-  if (isBrowser) {
-    plugins.push(require.resolve('babel-plugin-seek-style-guide'));
-  }
-
   if (isJest) {
     plugins.push(require.resolve('babel-plugin-dynamic-import-node'));
+  } else {
+    plugins.push(require.resolve('babel-plugin-seek-style-guide'));
   }
 
   if (isProductionBuild) {
@@ -79,7 +71,7 @@ module.exports = ({
     }
   }
 
-  const languagePreset =
+  const presets = [
     lang === 'ts'
       ? [
           require.resolve('@babel/preset-typescript'),
@@ -88,16 +80,15 @@ module.exports = ({
             allExtensions: true,
           },
         ]
-      : require.resolve('@babel/preset-flow');
+      : null,
+    [require.resolve('@babel/preset-env'), envPresetOptions],
+    require.resolve('@babel/preset-react'),
+  ].filter(Boolean);
 
   return {
     babelrc: false,
     sourceType: isBrowser ? 'unambiguous' : 'module',
-    presets: [
-      languagePreset,
-      [require.resolve('@babel/preset-env'), envPresetOptions],
-      require.resolve('@babel/preset-react'),
-    ],
+    presets,
     plugins,
   };
 };

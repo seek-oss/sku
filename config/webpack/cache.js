7aebd622418e2e08b255a809b8fc68c9b4ed2a35
Matt Jones <mattjones701@gmail.com>
2021-08-11 15:29:45 +1000

    Disable caching in CI

--- config/webpack/cache.js
+++ config/webpack/cache.js
@@ -1,7 +1,8 @@
 const { persistentCache, paths } = require('../../context');
+const isCI = require('../../lib/isCI');
 
 module.exports = function getWebpackCacheSettings({ isDevServer }) {
-  if (isDevServer) {
+  if (isDevServer && !isCI) {
     if (persistentCache) {
       return {
         type: 'filesystem',

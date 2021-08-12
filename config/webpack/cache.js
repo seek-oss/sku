ef5bf7cedde6a804477426d6a97ea5cc301ea22f
Matt Jones <mattjones701@gmail.com>
2021-08-12 14:28:33 +1000

    Remove CI flag from test script

--- config/webpack/cache.js
+++ config/webpack/cache.js
@@ -1,8 +1,10 @@
 const { persistentCache, paths } = require('../../context');
 const isCI = require('../../lib/isCI');
 
+const disableCacheOverride = Boolean(process.env.SKU_DISABLE_CACHE);
+
 module.exports = function getWebpackCacheSettings({ isDevServer }) {
-  if (isDevServer && !isCI) {
+  if (isDevServer && !isCI && !disableCacheOverride) {
     if (persistentCache) {
       return {
         type: 'filesystem',

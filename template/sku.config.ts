5fc7863e4eee7627eca881facfd575f018180d59
Ben Jervis <bjervis@seek.com.au>
2021-11-23 16:46:48 +1100

    Update template config file to be TS

--- template/sku.config.ts
+++ template/sku.config.ts
@@ -1,7 +1,11 @@
-module.exports = {
+import type { SkuConfig } from 'sku';
+
+const skuConfig: SkuConfig = {
   clientEntry: 'src/client.tsx',
   renderEntry: 'src/render.tsx',
   environments: ['development', 'production'],
   publicPath: '/path/to/public/assets/', // <-- Required for sku build output
   orderImports: true
 };
+
+export default skuConfig;

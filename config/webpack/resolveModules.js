a7d3834d1d252b8e380e83d1967831f1000f3813
Matt Jones <mattjones701@gmail.com>
2021-12-09 10:58:47 +1100

    Catch error

--- config/webpack/resolveModules.js
+++ config/webpack/resolveModules.js
@@ -1,13 +1,17 @@
-const skuPath = require.resolve('sku/package.json');
-
 const modules = ['node_modules'];
 
-// If the project is using pnpm then we add the sku node_modules directory
-// to the modules array. This allows dependecies of sku to be importable
-// as part of the build process. This is specifically useful for
-// @babel/plugin-transform-runtime as it inject imports to @babel/runtime.
-if (skuPath.includes('.pnpm')) {
-  modules.push(path.join(skuPath, '../..'));
+try {
+  const skuPath = require.resolve('sku/package.json');
+
+  // If the project is using pnpm then we add the sku node_modules directory
+  // to the modules array. This allows dependecies of sku to be importable
+  // as part of the build process. This is specifically useful for
+  // @babel/plugin-transform-runtime as it inject imports to @babel/runtime.
+  if (skuPath.includes('.pnpm')) {
+    modules.push(path.join(skuPath, '../..'));
+  }
+} catch (e) {
+  // Ignore errors as if sku isn't resolvable then we don't need to apply the pnpm fix
 }
 
 module.exports = modules;

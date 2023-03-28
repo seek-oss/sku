329eaaa3ea0685eb639ec39a4f99c4288a9bd994
Karl Juri Ots <kots@seek.com.au>
2023-03-28 16:46:07 +1100

    Add test for libraryFile config option.

---
+++ fixtures/library-file/sku.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  libraryEntry: 'src/library.js',
+  renderEntry: 'src/render.js',
+  libraryName: 'MyLibrary',
+  libraryFile: 'my-library',
+  port: 8086,
+  persistentCache: false,
+};
---
+++ fixtures/library-file/src/styles.treat.ts
@@ -0,0 +1,9 @@
+// @ts-ignore
+import { style } from 'sku/treat';
+
+export const customBox = style({
+  backgroundColor: 'lightseagreen',
+  color: 'white',
+  fontSize: 50,
+  padding: 20,
+});

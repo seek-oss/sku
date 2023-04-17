b673ef0710aab4e2d3996d0a3a8d7f072e0636c5
Adam Skoufis <askoufis@seek.com.au>
2023-04-17 11:00:41 +1000

    Update snapshots

--- fixtures/braid-design-system/src/App.test.tsx
+++ fixtures/braid-design-system/src/App.test.tsx
@@ -15,8 +15,8 @@ describe('braid-design-system', () => {
     ).toMatchInlineSnapshot(`
       "<style type="text/css">
                   html,body{margin:0;padding:0;background:#F7F8FB}
-                  html.sprinkles_darkMode__in90cbz,html.sprinkles_darkMode__in90cbz body{color-scheme:dark;background:#0E131B}
-                </style><div class="apacTheme_default__eah7240 typography_lightModeTone_light__myxsed18 typography_darkModeTone_dark__myxsed1b"><div class="reset_base__pv0jxi0 sprinkles_paddingTop_large_mobile__in90cb6q App_vanillaBox__inn18b0"></div></div>"
+                  html.sprinkles_darkMode__1xl4lgvz,html.sprinkles_darkMode__1xl4lgvz body{color-scheme:dark;background:#0E131B}
+                </style><div class="apacTheme_default__19awr0h0 typography_lightModeTone_light__1phx5d818 typography_darkModeTone_dark__1phx5d81b"><div class="reset_base__1di30x80 sprinkles_paddingTop_large_mobile__1xl4lgv6q App_vanillaBox__inn18b0"></div></div>"
     `);
   });
 });

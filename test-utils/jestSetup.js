e0d6eca0c77ba29185b3c997a9ebbe6ecee80d6b
Adam Skoufis <askoufis@seek.com.au>
2023-05-25 09:26:30 +1000

    Update `jest-puppeteer` and revert snapshot serializer refactor

--- test-utils/jestSetup.js
+++ test-utils/jestSetup.js
@@ -1,1 +1,9 @@
+const appSnapshotSerializer = require('./appSnapshotSerializer');
+const cssSnapshotSerializer = require('./cssSnapshotSerializer');
+const htmlSnapshotSerializer = require('./htmlSnapshotSerializer');
+
+expect.addSnapshotSerializer(appSnapshotSerializer);
+expect.addSnapshotSerializer(cssSnapshotSerializer);
+expect.addSnapshotSerializer(htmlSnapshotSerializer);
+
 jest.setTimeout(90000);

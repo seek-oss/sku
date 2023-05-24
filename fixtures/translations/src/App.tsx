c26009950ac6e541938dccb6fd5d47102c3b07c7
Remus Mate <rmate@seek.com.au>
2023-05-24 14:21:39 +1000

    add a fixture with `.mjs` files

--- fixtures/translations/src/App.tsx
+++ fixtures/translations/src/App.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import { useTranslations } from '@vocab/react';
 
 import translations from './App.vocab';
+// @ts-expect-error no types
+import compiledTranslations from './compiled.vocab/index.mjs';
 
 export default () => {
   const { t } = useTranslations(translations);
+  const compiled = useTranslations(compiledTranslations);
 
-  return <div>{t('hello')}</div>;
+  return (
+    <div>
+      {t('hello')} {compiled.t('Company name')}
+    </div>
+  );
 };

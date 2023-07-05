b904bd14155eceb0640f191e3eada83139984897
Remus Mate <rmate@seek.com.au>
2023-07-05 13:44:09 +1000

    add process utils for tests

--- test-utils/index.js
+++ test-utils/index.js
@@ -2,13 +2,12 @@ const { ListExternalsWebpackPlugin } = require('./ListExternalsWebpackPlugin');
 const appSnapshot = require('./appSnapshot');
 const { startAssetServer } = require('./assetServer');
 const { dirContentsToObject } = require('./dirContentsToObject');
+const { run, runSkuScriptInDir } = require('./process');
 const { makeStableHashes } = require('./skuConfig');
 const {
   getStorybookFrame,
   getTextContentFromStorybookFrame,
 } = require('./storybook');
-const { runSkuScriptInDir } = require('./runSkuScriptInDir');
-const { spawnSkuScriptInDir } = require('./spawnSkuScriptInDir');
 const { waitForUrls } = require('./waitForUrls');
 
 module.exports = {
@@ -19,7 +18,7 @@ module.exports = {
   getStorybookFrame,
   getTextContentFromStorybookFrame,
   makeStableHashes,
+  run,
   runSkuScriptInDir,
-  spawnSkuScriptInDir,
   waitForUrls,
 };

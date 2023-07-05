7e0d185161464ff0aca180e40a154bf7fe426f6a
Remus Mate <rmate@seek.com.au>
2023-07-05 12:46:44 +1000

    update failing test

--- fixtures/custom-src-paths/sku.config.ts
+++ fixtures/custom-src-paths/sku.config.ts
@@ -1,7 +1,9 @@
-module.exports = {
+import type { SkuConfig } from 'sku';
+
+export default {
   srcPaths: ['lib', 'another-folder'],
   clientEntry: 'lib/client.js',
   renderEntry: 'lib/render.js',
   port: 8201,
   publicPath: '/some-static-place',
-};
+} satisfies SkuConfig;

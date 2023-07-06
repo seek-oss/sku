0fd8cb13f3998e1f4765703471abfc89a0d8ceb8
Remus Mate <3297808+mrm007@users.noreply.github.com>
2023-07-06 15:49:18 +1000

    Add a workaround for TypeScript-ESLint being slow with TypeScript 5.1 (#854)

--- packages/sku/config/eslint/eslintConfig.js
+++ packages/sku/config/eslint/eslintConfig.js
@@ -6,6 +6,15 @@ const baseConfig = {
   extends: orderImports
     ? [coreConfig, require.resolve('./importOrderConfig')]
     : coreConfig,
+  // Temporarily configure @typescript-eslint/parser until eslint-config-seek is updated.
+  parserOptions: {
+    // https://github.com/typescript-eslint/typescript-eslint/issues/6544
+    allowAutomaticSingleRunInference: true,
+    ecmaVersion: 2022,
+    project: true,
+    sourceType: 'module',
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
   rules: {
     // Temporarily disable this rule until eslint-config-seek is updated.
     // This goes hand-in-hand with `compilerOptions.jsx: 'react-jsx'` in tsconfig.json

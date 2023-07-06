8888307ff3d0240d457bb437bd7a1df4378e7076
Remus Mate <rmate@seek.com.au>
2023-07-06 11:12:29 +1000

    typescript errors

--- fixtures/typescript-css-modules/sku-ssr.config.ts
+++ fixtures/typescript-css-modules/sku-ssr.config.ts
@@ -1,4 +1,5 @@
 import type { SkuConfig } from 'sku';
+// @ts-ignore no types
 import { makeStableHashes } from '@sku-private/test-utils';
 
 export default {
--- test-utils/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function makeStableHashes(config: any): any;

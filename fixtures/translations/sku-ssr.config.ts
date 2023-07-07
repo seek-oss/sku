2e924ead0296062873d284301203feb433589973
Remus Mate <3297808+mrm007@users.noreply.github.com>
2023-07-07 14:54:37 +1000

    Bump eslint-config-seek (#855)

--- fixtures/translations/sku-ssr.config.ts
+++ fixtures/translations/sku-ssr.config.ts
@@ -1,4 +1,6 @@
 import type { SkuConfig } from 'sku';
+// @ts-ignore no types
+import { makeStableHashes } from '@sku-private/test-utils';
 
 export default {
   clientEntry: 'src/ssr-client.tsx',
@@ -9,4 +11,5 @@ export default {
   target: 'dist-ssr',
   port: 8313,
   serverPort: 8314,
+  dangerouslySetWebpackConfig: (config) => makeStableHashes(config),
 } satisfies SkuConfig;
--- fixtures/typescript-css-modules/src/server.tsx
+++ fixtures/typescript-css-modules/src/server.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import type React from 'react';
 import { renderToString } from 'react-dom/server';
 
 import App from './App';

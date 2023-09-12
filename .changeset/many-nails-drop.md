a65bcdeaf0d316f1d4582cfea4fd70814ffc1eef
Adam Skoufis <askoufis@users.noreply.github.com>
2023-09-12 10:15:13 +1000

    Update webpack-related deps, clean up some deps (#885)

---
+++ .changeset/many-nails-drop.md
@@ -0,0 +1,5 @@
+---
+'sku': patch
+---
+
+Bump `@pmmmwh/react-refresh-webpack-plugin` and `webpack-dev-server` dependencies. Remove unused dependencies.
--- tests/typescript-css-modules.test.ts
+++ tests/typescript-css-modules.test.ts
@@ -1,4 +1,4 @@
-import assert from 'assert';
+import assert from 'node:assert/strict';
 import path from 'path';
 import { rimraf } from 'rimraf';
 import {

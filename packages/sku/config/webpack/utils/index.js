610194b374464cc503ef3f0418cf95b83d0306ae
Adam Skoufis <askoufis@seek.com.au>
2023-11-25 12:38:09 +1100

    Update JS and TS file extension regexes

--- packages/sku/config/webpack/utils/index.js
+++ packages/sku/config/webpack/utils/index.js
@@ -1,11 +1,12 @@
 const loaders = require('./loaders');
 const { resolvePackage } = require('./resolvePackage');
+const { js, ts } = require('eslint-config-seek/extensions');
 
 module.exports = {
   ...loaders,
   resolvePackage,
-  TYPESCRIPT: /\.(ts|tsx)$/,
-  JAVASCRIPT: /\.(js|cjs|mjs)$/,
+  TYPESCRIPT: new RegExp(`\.(${ts.join('|')})$`),
+  JAVASCRIPT: new RegExp(`\.(${js.join('|')})$`),
   LESS: /\.less$/,
   IMAGE: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
   SVG: /\.svg$/,

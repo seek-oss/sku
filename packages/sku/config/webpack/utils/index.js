00332e840d8fd03f9fcc2536a5203a1d11773fd7
Adam Skoufis <askoufis@users.noreply.github.com>
2023-11-27 16:29:53 +1100

    Run `.cjs` and `.mjs` files through preset env (#906)

--- packages/sku/config/webpack/utils/index.js
+++ packages/sku/config/webpack/utils/index.js
@@ -1,11 +1,18 @@
 const loaders = require('./loaders');
 const { resolvePackage } = require('./resolvePackage');
 
+// TODO: Figure out a better way to share this config. This is currently copy-pasted from
+// `eslint-config-seek`
+const extensions = {
+  js: ['js', 'cjs', 'mjs', 'jsx'],
+  ts: ['ts', 'cts', 'mts', 'tsx'],
+};
+
 module.exports = {
   ...loaders,
   resolvePackage,
-  TYPESCRIPT: /\.(ts|tsx)$/,
-  JAVASCRIPT: /\.js$/,
+  TYPESCRIPT: new RegExp(`\.(${extensions.ts.join('|')})$`),
+  JAVASCRIPT: new RegExp(`\.(${extensions.js.join('|')})$`),
   LESS: /\.less$/,
   IMAGE: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
   SVG: /\.svg$/,

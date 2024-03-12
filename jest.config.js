35d4ec165e275abbd79fe18ddf5c05b444f0a2d7
Adam Skoufis <askoufis@seek.com.au>
2024-03-12 13:13:13 +1100

    Set `maxWorkers=100%` on CI

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     '^.+\\.(t|j)sx?$': '@swc/jest',
   },
   preset: 'jest-puppeteer',
+  ...(process.env.CI ? { maxWorkers: '100%' } : {}),
   setupFilesAfterEnv: ['<rootDir>/test-utils/jestSetup.ts'],
   snapshotSerializers: [
     '<rootDir>/test-utils/appSnapshotSerializer.js',

9476de4c49596ca4170102888e8a95c1800dd58c
Adam Skoufis <askoufis@users.noreply.github.com>
2024-08-13 14:15:47 +1000

    Refine storybook webpack config to fix a bug preventing MDX files from loading in storybook (#1032)

--- fixtures/storybook-config/.storybook/main.ts
+++ fixtures/storybook-config/.storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 import { babel, webpackFinal } from 'sku/config/storybook';
 
 export default {
-  stories: ['../src/**/*.stories.tsx'],
+  stories: ['../src/**/*.stories.tsx', '../src/**/*.mdx'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {
@@ -14,7 +14,7 @@ export default {
   core: {
     disableTelemetry: true,
   },
-  addons: ['@storybook/addon-webpack5-compiler-babel'],
+  addons: ['@storybook/addon-webpack5-compiler-babel', '@storybook/addon-docs'],
   babel,
   webpackFinal,
 } satisfies StorybookConfig;

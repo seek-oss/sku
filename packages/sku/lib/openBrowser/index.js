da1e8877a6d06e3d2591b9168ef8beea81b18322
Felix Habib <fhabib@seek.com.au>
2024-08-19 11:44:56 +1000

    Support other chromium browsers

--- packages/sku/lib/openBrowser/index.js
+++ packages/sku/lib/openBrowser/index.js
@@ -12,22 +12,39 @@ module.exports = (url) => {
   if (process.env.OPEN_TAB !== 'false' && !isCI) {
     const browser = process.env.BROWSER;
 
-    const shouldTryOpenChromeWithAppleScript =
+    const shouldTryOpenChromiumWithAppleScript =
       process.platform === 'darwin' &&
       (typeof browser !== 'string' || browser === OSX_CHROME);
 
-    if (shouldTryOpenChromeWithAppleScript) {
-      try {
-        // Try our best to reuse existing tab
-        // on OS X Google Chrome with AppleScript
-        execSync('ps cax | grep "Google Chrome"');
-        execSync(`osascript openChrome.applescript "${encodeURI(url)}"`, {
-          cwd: __dirname,
-          stdio: 'ignore',
-        });
-        return;
-      } catch (e) {
-        // Ignore errors.
+    if (shouldTryOpenChromiumWithAppleScript) {
+      // Will use the first open browser found from list
+      const supportedChromiumBrowsers = [
+        'Google Chrome Canary',
+        'Google Chrome',
+        'Microsoft Edge',
+        'Brave Browser',
+        'Vivaldi',
+        'Chromium',
+      ];
+
+      for (const chromiumBrowser of supportedChromiumBrowsers) {
+        try {
+          // Try our best to reuse existing tab
+          // on OS X Google Chrome with AppleScript
+          execSync(`ps cax | grep "${chromiumBrowser}"`);
+          execSync(
+            `osascript openChrome.applescript "${encodeURI(
+              url,
+            )}" "${chromiumBrowser}"`,
+            {
+              cwd: __dirname,
+              stdio: 'ignore',
+            },
+          );
+          return true;
+        } catch (e) {
+          // Ignore errors.
+        }
       }
     }
 

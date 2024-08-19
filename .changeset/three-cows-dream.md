5238b4a961e365f9f16614b5a2e3d7d5a4128122
Felix Habib <fhabib@seek.com.au>
2024-08-19 11:33:10 +1000

    Refactor current solution

---
+++ .changeset/three-cows-dream.md
@@ -0,0 +1,5 @@
+---
+'sku': patch
+---
+
+dummy to force snapshot publish
--- packages/sku/lib/openBrowser/index.js
+++ packages/sku/lib/openBrowser/index.js
@@ -2,32 +2,35 @@
 // https://github.com/facebook/create-react-app/blob/master/packages/react-dev-utils/openBrowser.js
 
 const execSync = require('node:child_process').execSync;
-const defaultBrowser = require('x-default-browser');
 const open = require('open');
 
 const isCI = require('../isCI');
 
+const OSX_CHROME = 'google chrome';
+
 module.exports = (url) => {
   if (process.env.OPEN_TAB !== 'false' && !isCI) {
-    defaultBrowser((err, res) => {
-      const useChrome = err ? false : res.isChrome;
+    const browser = process.env.BROWSER;
 
-      if (process.platform === 'darwin' && useChrome) {
-        try {
-          // Try our best to reuse existing tab
-          // on OS X Google Chrome with AppleScript
-          execSync('ps cax | grep "Google Chrome"');
-          execSync(`osascript openChrome.applescript "${encodeURI(url)}"`, {
-            cwd: __dirname,
-            stdio: 'ignore',
-          });
-          return;
-        } catch (e) {
-          // Ignore errors.
-        }
+    const shouldTryOpenChromeWithAppleScript =
+      process.platform === 'darwin' &&
+      (typeof browser !== 'string' || browser === OSX_CHROME);
+
+    if (shouldTryOpenChromeWithAppleScript) {
+      try {
+        // Try our best to reuse existing tab
+        // on OS X Google Chrome with AppleScript
+        execSync('ps cax | grep "Google Chrome"');
+        execSync(`osascript openChrome.applescript "${encodeURI(url)}"`, {
+          cwd: __dirname,
+          stdio: 'ignore',
+        });
+        return;
+      } catch (e) {
+        // Ignore errors.
       }
+    }
 
-      open(url, { url: true });
-    });
+    open(url, { url: true });
   }
 };

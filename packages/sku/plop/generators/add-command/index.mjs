bd2140fa06e0bdd8b779c5ab1cc468197a4aaf90
Dan Drory <ddrory@seek.com.au>
2024-11-26 14:00:37 +1100

    fix: Prettier fixes

--- packages/sku/plop/generators/add-command/index.mjs
+++ packages/sku/plop/generators/add-command/index.mjs
@@ -1,10 +1,11 @@
-import fs from "node:fs";
+import fs from 'node:fs';
 
 const getCommandsList = () => {
-  const commands = fs.readdirSync('./lib/program/commands', { encoding: 'utf-8' });
+  const commands = fs.readdirSync('./lib/program/commands', {
+    encoding: 'utf-8',
+  });
   return commands.filter((command) => command !== 'index.js');
-}
-
+};
 
 const addCommandGenerator = (plop) => {
   const subCommands = getCommandsList();
@@ -32,60 +33,69 @@ const addCommandGenerator = (plop) => {
     ],
     actions: (data) => {
       // check if parent has subcommands.
-      const hasExistingSubCommands=  fs.existsSync(`./lib/program/commands/${data.parentCommand}/commands`);
+      const hasExistingSubCommands = fs.existsSync(
+        `./lib/program/commands/${data.parentCommand}/commands`,
+      );
 
-      console.log('parentCommand', data)
+      console.log('parentCommand', data);
 
       return [
         {
           type: 'add',
           templateFile: './plop/generators/add-command/templates/command.hbs',
-          path: `./lib/program/commands/${data.isSubCommand ? `{{parentCommand}}/commands/` : ''}{{commandName}}/{{commandName}}.command.js`,
+          path: `./lib/program/commands/${
+            data.isSubCommand ? `{{parentCommand}}/commands/` : ''
+          }{{commandName}}/{{commandName}}.command.js`,
         },
         {
           type: 'add',
           templateFile: './plop/generators/add-command/templates/action.hbs',
-          path: `./lib/program/commands/${data.isSubCommand ? `{{parentCommand}}/commands/` : ''}{{commandName}}/{{commandName}}.action.js`,
+          path: `./lib/program/commands/${
+            data.isSubCommand ? `{{parentCommand}}/commands/` : ''
+          }{{commandName}}/{{commandName}}.action.js`,
         },
         ...(!hasExistingSubCommands
-          ? [{
-              type: 'add',
-              templateFile: './plop/generators/add-command/templates/command-index.hbs',
-              path: `./lib/program/commands/{{parentCommand}}/commands/index.js`,
-            },
-            {
-              type: 'modify',
-              path: `./lib/program/commands/{{parentCommand}}/{{parentCommand}}.command.js`,
-              pattern: /(const \{ Command } = require\('commander'\);)/g,
-              template:
-                "$1\nconst commands = require('./commands');",
-            },
-            {
-              type: 'modify',
-              path: `./lib/program/commands/{{parentCommand}}/{{parentCommand}}.command.js`,
-              pattern: /(module\.exports =)/g,
-              template:
-                `for (const command of commands) {\n  {{camelCase parentCommand}}.addCommand(command);\n}\n\n$1`,
-            }]
-          : []
-        ),
+          ? [
+              {
+                type: 'add',
+                templateFile:
+                  './plop/generators/add-command/templates/command-index.hbs',
+                path: `./lib/program/commands/{{parentCommand}}/commands/index.js`,
+              },
+              {
+                type: 'modify',
+                path: `./lib/program/commands/{{parentCommand}}/{{parentCommand}}.command.js`,
+                pattern: /(const \{ Command } = require\('commander'\);)/g,
+                template: "$1\nconst commands = require('./commands');",
+              },
+              {
+                type: 'modify',
+                path: `./lib/program/commands/{{parentCommand}}/{{parentCommand}}.command.js`,
+                pattern: /(module\.exports =)/g,
+                template: `for (const command of commands) {\n  {{camelCase parentCommand}}.addCommand(command);\n}\n\n$1`,
+              },
+            ]
+          : []),
         {
           type: 'modify',
-          path: `./lib/program/commands/${data.isSubCommand ? `{{parentCommand}}/commands/` : ''}index.js`,
+          path: `./lib/program/commands/${
+            data.isSubCommand ? `{{parentCommand}}/commands/` : ''
+          }index.js`,
           pattern: /(\/\* \[add-command-generator: import] \*\/)/g,
           template:
             "const {{camelCase commandName}}Command = require('./{{commandName}}/{{commandName}}.command.js');\n$1",
         },
         {
           type: 'modify',
-          path: `./lib/program/commands/${data.isSubCommand ? `{{parentCommand}}/commands/` : ''}index.js`,
+          path: `./lib/program/commands/${
+            data.isSubCommand ? `{{parentCommand}}/commands/` : ''
+          }index.js`,
           pattern: /(\/\* \[add-command-generator: invocation] \*\/)/g,
-          template:
-            "{{camelCase commandName}}Command,\n  $1",
+          template: '{{camelCase commandName}}Command,\n  $1',
         },
-      ]
+      ];
     },
   });
-}
+};
 
 export default addCommandGenerator;
--- packages/sku/plop/helpers/camel-case-helper.mjs
+++ packages/sku/plop/helpers/camel-case-helper.mjs
@@ -3,7 +3,7 @@ import camelCase from 'lodash/camelCase.js';
 const camelCaseHelper = (plop) => {
   plop.setHelper('camelCase', (text) => {
     return camelCase(text);
-  })
-}
+  });
+};
 
 export default camelCaseHelper;

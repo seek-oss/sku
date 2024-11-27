91d700dbf3c5af9c0b9f17398abd49407d0b5d56
Dan Drory <ddrory@seek.com.au>
2024-11-27 12:27:01 +1100

    update changeset and environment option

--- packages/sku/lib/program/options/environment/environment.option.js
+++ packages/sku/lib/program/options/environment/environment.option.js
@@ -2,6 +2,6 @@ const { Option } = require('commander');
 const environmentOption = new Option(
   '-e, --environment [environment]',
   'Node environment to run in',
-);
+).choices(['development', 'production', 'test']);
 
 module.exports = environmentOption;

a95289b6d9e07fccafeac3c2f8f2c306a45c4e26
Dan Drory <101684796+DanDroryAu@users.noreply.github.com>
2024-12-03 09:58:35 +1100

    Migrate `build` command and action to mjs (#1109)

--- packages/sku/lib/program/commands/build/build.command.mjs
+++ packages/sku/lib/program/commands/build/build.command.mjs
@@ -1,16 +1,16 @@
 const { Command } = require('commander');
 const statsOption = require('../../options/stats/stats.option');
 
-const build = new Command('build');
+const buildCommand = new Command('build');
 
-build
+buildCommand
   .description(
     'Create a production build of a statically-rendered application.',
   )
   .addOption(statsOption)
-  .action(({ stats }) => {
-    const buildAction = require('./build.action');
+  .action(async ({ stats }) => {
+    const { buildAction } = await import('./build.action');
     buildAction({ stats });
   });
 
-module.exports = build;
+export { buildCommand };

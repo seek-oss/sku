306f796e7adafe994c2bb25b6d3263806741904c
Dan Drory <ddrory@seek.com.au>
2024-12-10 16:29:44 +1100

    update jest config to exclude sku-init from main tests

--- test-utils/gracefulSpawn.js
+++ test-utils/gracefulSpawn.js
@@ -1,25 +1,29 @@
 import { promisify } from 'node:util';
-import { spawn } from 'cross-spawn';
+import spawn from 'cross-spawn';
 import treeKill from 'tree-kill';
 import onDeath from 'death';
 
 const treeKillAsync = promisify(treeKill);
 
 export const gracefulSpawn = (...args) => {
-  const childProcess = spawn(...args);
+  try {
+    const childProcess = spawn(...args);
 
-  childProcess.kill = async (signal) => {
-    await treeKillAsync(childProcess.pid, signal);
+    childProcess.kill = async (signal) => {
+      await treeKillAsync(childProcess.pid, signal);
 
-    // Needs a bit more time, for some reason :(
-    // If we don't give it a bit of breathing room,
-    // Jest complains about handles being left open.
-    await new Promise((resolve) => setTimeout(resolve, 100));
-  };
+      // Needs a bit more time, for some reason :(
+      // If we don't give it a bit of breathing room,
+      // Jest complains about handles being left open.
+      await new Promise((resolve) => setTimeout(resolve, 100));
+    };
 
-  onDeath((signal) => {
-    childProcess.kill(signal);
-  });
+    onDeath((signal) => {
+      childProcess.kill(signal);
+    });
 
-  return childProcess;
+    return childProcess;
+  } catch (e) {
+    console.log('cross spawn issue', e);
+  }
 };

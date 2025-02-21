1f81ad8f5e911536a1032d07665a36986d4e4bf8
Adam Skoufis <askoufis@users.noreply.github.com>
2025-02-21 16:33:32 +1100

    Fix `webp` and `avif` support, test asset loading in fixture (#1181)

--- fixtures/public-path/src/app.js
+++ fixtures/public-path/src/app.js
@@ -1,3 +1,12 @@
 import * as styles from './app.css.js';
 
-export default () => <div className={styles.root} />;
+export default () => (
+  <>
+    <div className={styles.avif} />
+    <div className={styles.bmp} />
+    <div className={styles.gif} />
+    <div className={styles.jpg} />
+    <div className={styles.png} />
+    <div className={styles.webp} />
+  </>
+);

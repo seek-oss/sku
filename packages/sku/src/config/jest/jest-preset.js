13088abfb69ce826d4656d89d71b6749de25af61
Adam Skoufis <askoufis@seek.com.au>
2025-02-21 16:37:51 +1100

    Fix missing support for `.bmp` file mocking in jest tests

--- packages/sku/src/config/jest/jest-preset.js
+++ packages/sku/src/config/jest/jest-preset.js
@@ -27,7 +27,7 @@ export default jestDecorator({
   ],
   moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|svg|avif)$':
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|svg|avif|bmp)$':
       fileURLToPath(import.meta.resolve('./fileMock.cjs')),
   },
   transform: {

7d42c8e1bf662131eea40e23f78e924d8044f19e
Adam Skoufis <askoufis@seek.com.au>
2025-02-27 15:43:40 +1100

    `sku test`: Add a regresssion test for passing through args to jest

--- tests/sku-test.test.ts
+++ tests/sku-test.test.ts
@@ -1,6 +1,7 @@
 import path from 'node:path';
 import { runSkuScriptInDir } from '@sku-private/test-utils';
 import { createRequire } from 'node:module';
+import { cwd } from 'node:process';
 
 const require = createRequire(import.meta.url);
 
@@ -8,7 +9,16 @@ const appDir = path.dirname(
   require.resolve('@sku-fixtures/sku-test/sku.config.ts'),
 );
 
-test('sku test', async () => {
-  const { child } = await runSkuScriptInDir('test', appDir);
-  expect(child.exitCode).toEqual(0);
+describe('sku-test', () => {
+  it('should run tests', async () => {
+    const { child } = await runSkuScriptInDir('test', appDir);
+    expect(child.exitCode).toEqual(0);
+  });
+
+  it('should pass through unknown flags to jest', async () => {
+    const { stdout } = await runSkuScriptInDir('test', appDir, ['--listTests']);
+    const output = stdout.replaceAll(cwd(), 'sku');
+
+    expect(output).toMatchSnapshot();
+  });
 });

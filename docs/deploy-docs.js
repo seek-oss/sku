d9fd8aca5d965fb972489b9bf6e7e5bf756b0330
Adam Skoufis <askoufis@seek.com.au>
2025-03-13 16:03:32 +1100

    Fix deployment

--- docs/deploy-docs.js
+++ docs/deploy-docs.js
@@ -1,6 +1,10 @@
-const ghpages = require('gh-pages');
+import { publish } from 'gh-pages';
 
-const repoUrl = require('../package.json').repository.url;
+import packageJson from '../package.json' with { type: 'json' };
+
+const {
+  repository: { url: repoUrl },
+} = packageJson;
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const tokenRegex = GITHUB_TOKEN ? new RegExp(GITHUB_TOKEN, 'g') : null;
@@ -11,17 +15,15 @@ const log = function (message) {
   );
 };
 
-const makeConfig = function () {
-  return {
-    repo: GITHUB_TOKEN
-      ? repoUrl.replace('https://', `https://${GITHUB_TOKEN}@`)
-      : repoUrl,
-    logger: log,
-    dotfiles: true,
-  };
+const publishConfig = {
+  repo: GITHUB_TOKEN
+    ? repoUrl.replace('git+https://', `https://${GITHUB_TOKEN}@`)
+    : repoUrl,
+  logger: log,
+  dotfiles: true,
 };
 
-ghpages.publish('.', makeConfig(), function (err) {
+await publish('.', publishConfig, function (err) {
   if (err) {
     log('Deployment error');
     log(JSON.stringify(err));

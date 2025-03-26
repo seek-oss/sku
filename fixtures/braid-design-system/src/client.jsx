32b11236d7b87afb2c35a14935b092bc146fb100
Adam Skoufis <askoufis@seek.com.au>
2025-03-26 14:41:16 +1100

    Fix formatting

--- fixtures/braid-design-system/src/client.jsx
+++ fixtures/braid-design-system/src/client.jsx
@@ -2,5 +2,5 @@ import { hydrateRoot } from 'react-dom/client';
 
 import App from './App';
 
-export default ({ site}) =>
+export default ({ site }) =>
   hydrateRoot(document.getElementById('app'), <App themeName={site} />);

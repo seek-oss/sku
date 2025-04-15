bed69b92b0a4f60fc9d2c5a3707f826ce57581a9
William Lark <wlark@seek.com.au>
2025-04-15 18:17:55 +1000

    fix: vite plugin path matching hits valid routes only

--- packages/sku/src/services/vite/plugins/skuViteMiddlewarePlugin.ts
+++ packages/sku/src/services/vite/plugins/skuViteMiddlewarePlugin.ts
@@ -20,82 +20,91 @@ const clientEntry = require.resolve('../entries/vite-client.js');
 export const skuViteMiddlewarePlugin = (skuContext: SkuContext): Plugin => ({
   name: 'vite-plugin-sku-server-middleware',
   async configureServer(server) {
-    if (metricsMeasurers.initialPageLoad.isInitialPageLoad) {
-      metricsMeasurers.initialPageLoad.mark();
-    }
-    if (skuContext.useDevServerMiddleware) {
-      log(
-        'Using dev server middleware at %s',
-        skuContext.paths.devServerMiddleware,
-      );
-      const devServerMiddleware = (
-        await import(skuContext.paths.devServerMiddleware)
-      ).default;
-      if (devServerMiddleware && typeof devServerMiddleware === 'function') {
-        devServerMiddleware(server);
-        log('Dev server middleware loaded');
+    return async () => {
+      if (metricsMeasurers.initialPageLoad.isInitialPageLoad) {
+        metricsMeasurers.initialPageLoad.mark();
       }
-    }
-
-    log('Configuring server middleware');
-    server.middlewares.use(async (req, res, next) => {
-      log('Handling request:', req.url);
-      if (!req.url) {
-        log('No url found for request. Skipping render.');
-        next();
-        return;
-      }
-
-      // `host` header is available in vite http requests. `:authority` pseudo-header is available in vite https requests. `:authority` is used in HTTP/2.
-      // @see https://stackoverflow.com/questions/70502726/what-is-the-purpose-of-http2-pseudo-headers-authority-method
-      const host = req.headers.host ?? (req.headers[':authority'] as string);
-      const hostname = host?.split(':')[0];
-
-      if (!hostname) {
-        throw new Error(
-          'Internal error occurred. No hostname found when handling middleware. Host header required.',
+      if (skuContext.useDevServerMiddleware) {
+        log(
+          'Using dev server middleware at %s',
+          skuContext.paths.devServerMiddleware,
         );
-      }
-      const site =
-        skuContext.sites.find((skuSite) => skuSite.host === hostname) ||
-        skuContext.sites[0];
-
-      const matchingRoute = getMatchingRoute({
-        hostname: host,
-        path: req.url,
-        routes: skuContext.routes,
-        sites: skuContext.sites,
-      });
-
-      if (!matchingRoute) {
-        log('No matching route found for request. Skipping render.');
-        next();
-        return;
+        const devServerMiddleware = (
+          await import(skuContext.paths.devServerMiddleware)
+        ).default;
+        if (devServerMiddleware && typeof devServerMiddleware === 'function') {
+          devServerMiddleware(server);
+          log('Dev server middleware loaded');
+        }
       }
 
-      log('Matching route found:', matchingRoute);
+      log('Configuring server middleware');
+      server.middlewares.use(async (req, res, next) => {
+        log('Handling request:', req.url);
+        // Check that the request is for the index.html file (i.e., a valid route in our app)
+        // If we don't check for index.html, the middleware will run for every request, including static assets.
+        if (!req.originalUrl || req.url !== '/index.html') {
+          log('No url found for request. Skipping render.');
+          next();
+          return;
+        }
 
-      const language =
-        getLanguageFromRoute(req.url, matchingRoute, skuContext) || '';
+        // `host` header is available in vite http requests. `:authority` pseudo-header is available in vite https requests. `:authority` is used in HTTP/2.
+        // @see https://stackoverflow.com/questions/70502726/what-is-the-purpose-of-http2-pseudo-headers-authority-method
+        const host = req.headers.host ?? (req.headers[':authority'] as string);
+        const hostname = host?.split(':')[0];
 
-      const { viteRender } = await server.ssrLoadModule(renderEntry);
+        if (!hostname) {
+          throw new Error(
+            'Internal error occurred. No hostname found when handling middleware. Host header required.',
+          );
+        }
+        const site =
+          skuContext.sites.find((skuSite) => skuSite.host === hostname) ||
+          skuContext.sites[0];
 
-      const renderedHtml = await (viteRender as ViteRenderFunction)({
-        environment: 'development',
-        language,
-        route: getRouteWithLanguage(matchingRoute.route, language),
-        routeName: matchingRoute.name || '',
-        site: site?.name || '',
-        clientEntry,
+        // Reconstruct the URL to get the pathname since it's not available in the request object
+        // `req.originalUrl` is the full URL path, including query parameters.
+        const url = new URL(req.originalUrl, `http://${host}`);
+        const { pathname: path } = url;
+
+        const matchingRoute = getMatchingRoute({
+          hostname: host,
+          path,
+          routes: skuContext.routes,
+          sites: skuContext.sites,
+        });
+
+        if (!matchingRoute) {
+          log('No matching route found for request. Skipping render.');
+          next();
+          return;
+        }
+
+        log('Matching route found:', matchingRoute);
+
+        const language =
+          getLanguageFromRoute(path, matchingRoute, skuContext) ?? '';
+
+        const { viteRender } = await server.ssrLoadModule(renderEntry);
+
+        const renderedHtml = await (viteRender as ViteRenderFunction)({
+          environment: 'development',
+          language,
+          route: getRouteWithLanguage(matchingRoute.route, language),
+          routeName: matchingRoute.name || '',
+          site: site?.name || '',
+          clientEntry,
+        });
+
+        const transformedHtml = await server.transformIndexHtml(
+          req.url || '/',
+          renderedHtml,
+        );
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(transformedHtml);
       });
-
-      const transformedHtml = await server.transformIndexHtml(
-        req.url || '/',
-        renderedHtml,
-      );
-
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(transformedHtml);
-    });
+    };
   },
 });

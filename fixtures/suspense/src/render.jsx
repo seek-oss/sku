03cb150ed05e0874c087926bc23eca72c522b57d
Adam Skoufis <askoufis@seek.com.au>
2025-06-24 13:40:36 +1000

    Set deterministic dehydratedState timestamp for snapshot consistency
    - Seems like react query added a new `dehydratedAt` property

--- fixtures/suspense/src/render.jsx
+++ fixtures/suspense/src/render.jsx
@@ -28,6 +28,7 @@ export default {
 
     // Set deterministic future dataUpdatedAt for snapshot consistency
     dehydratedState.queries[0].state.dataUpdatedAt = 4323283200000;
+    dehydratedState.queries[0].dehydratedAt = 4323283200000;
 
     return {
       appHtml,

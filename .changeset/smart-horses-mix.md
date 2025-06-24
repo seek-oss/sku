f223407b70d61868fa1442b950ad64cc24049978
William Lark <willjlark@gmail.com>
2025-06-24 11:38:29 +1000

    Fix tab opening (#1288)
    Co-authored-by: Adam Skoufis <askoufis@users.noreply.github.com>

---
+++ .changeset/smart-horses-mix.md
@@ -0,0 +1,7 @@
+---
+'sku': patch
+---
+
+`start`: Properly reuses Chrome tabs when starting a server.
+
+Fixes an issue of always opening a new tab in Chrome when one could be reused.
--- packages/sku/src/openBrowser/index.ts
+++ packages/sku/src/openBrowser/index.ts
@@ -2,10 +2,17 @@
 // https://github.com/facebook/create-react-app/commit/d2de54b25cc25800df1764058997e3e274bd79ac
 
 import chalk from 'chalk';
-import { execSync } from 'node:child_process';
+import { type ExecOptions, exec } from 'node:child_process';
 import open from 'open';
 import isCI from '@/utils/isCI.js';
 import getDefaultBrowser from 'default-browser';
+import { fileURLToPath } from 'node:url';
+import { resolve } from 'node:path';
+import debug from 'debug';
+
+const log = debug('sku:openBrowser');
+
+export const BIN_DIR = resolve(fileURLToPath(import.meta.url), '../../../bin');
 
 const OSX_CHROME = 'google chrome';
 
@@ -21,59 +28,76 @@ const supportedChromiumBrowsers = [
   'Arc',
 ];
 
-export const openBrowser = async (url: string) => {
-  if (process.env.OPEN_TAB !== 'false' && !isCI) {
-    let defaultBrowser = ''; // Has to be set to string otherwise it may be undefined on line 47.
-    try {
-      const { name } = await getDefaultBrowser();
-      defaultBrowser = name;
-    } catch {
-      console.log(chalk.yellow.bold('Failed to detect default browser.'));
-      console.log(
-        chalk.yellow(
-          `For a better ${chalk.italic('start')} experience on macOS, go to ${chalk.italic(
-            'System Preferences > Privacy & Security > Automation > Terminal/Application',
-          )} and enable Finder permissions.`,
-        ),
-      );
-    }
+async function getBrowserPreference() {
+  const envBrowser =
+    process.env.BROWSER === OSX_CHROME
+      ? 'Google Chrome'
+      : (process.env.BROWSER ?? '');
 
-    const availableBrowser = process.env.BROWSER;
-
-    const shouldTryOpenChromiumWithAppleScript =
-      process.platform === 'darwin' &&
-      (typeof availableBrowser !== 'string' ||
-        availableBrowser === OSX_CHROME) &&
-      supportedChromiumBrowsers.includes(defaultBrowser);
-
-    if (shouldTryOpenChromiumWithAppleScript) {
-      // Will use the first open browser found from list
-      const supportedChromiumBrowsersByPreference = new Set([
-        defaultBrowser,
-        ...supportedChromiumBrowsers,
-      ]);
-
-      for (const chromiumBrowser of supportedChromiumBrowsersByPreference) {
-        try {
-          // Try our best to reuse existing tab
-          // on OS X Google Chrome with AppleScript
-          execSync(`ps cax | grep "${chromiumBrowser}"`);
-          execSync(
-            `osascript openChrome.applescript "${encodeURI(
-              url,
-            )}" "${chromiumBrowser}"`,
-            {
-              cwd: __dirname,
-              stdio: 'ignore',
-            },
-          );
-          return true;
-        } catch {
-          // Ignore errors.
-        }
-      }
-    }
-
-    open(url);
+  try {
+    const { name } = await getDefaultBrowser();
+    return name ?? envBrowser;
+  } catch {
+    console.log(chalk.yellow.bold('Failed to detect default browser.'));
+    console.log(
+      chalk.yellow(
+        `For a better ${chalk.italic('start')} experience on macOS, go to ${chalk.italic(
+          'System Preferences > Privacy & Security > Automation > Terminal/Application',
+        )} and enable Finder permissions.`,
+      ),
+    );
+    return envBrowser;
   }
+}
+
+export const openBrowser = async (url: string) => {
+  if (process.env.OPEN_TAB === 'false' || isCI) {
+    return;
+  }
+
+  const preferredBrowser = await getBrowserPreference();
+
+  const shouldTryOpenChromiumWithAppleScript =
+    process.platform === 'darwin' &&
+    (!preferredBrowser || supportedChromiumBrowsers.includes(preferredBrowser));
+
+  if (shouldTryOpenChromiumWithAppleScript) {
+    try {
+      const ps = await execAsync('ps cax');
+      const openedBrowser =
+        preferredBrowser && ps.includes(preferredBrowser)
+          ? preferredBrowser
+          : supportedChromiumBrowsers.find((b) => ps.includes(b));
+
+      if (openedBrowser) {
+        // Try our best to reuse existing tab with AppleScript
+        await execAsync(
+          `osascript openChrome.applescript "${encodeURI(
+            url,
+          )}" "${openedBrowser}"`,
+          {
+            cwd: BIN_DIR,
+          },
+        );
+        return true;
+      }
+    } catch (error) {
+      // Ignore errors and just log them
+      log('Failed to open browser with AppleScript:', error);
+    }
+  }
+  log('Opening in a new tab in the browser:', preferredBrowser);
+  open(url);
 };
+
+function execAsync(command: string, options?: ExecOptions): Promise<string> {
+  return new Promise((res, rej) => {
+    exec(command, options, (error, stdout) => {
+      if (error) {
+        rej(error);
+      } else {
+        res(stdout.toString());
+      }
+    });
+  });
+}

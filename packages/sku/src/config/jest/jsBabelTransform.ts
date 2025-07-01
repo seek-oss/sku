562beb2b7bf0b69cbf458958b87f5a19a2314c3b
Adam Skoufis <askoufis@seek.com.au>
2025-07-01 14:50:02 +1000

    Convert jest config files to TypeScript

--- packages/sku/src/config/jest/jsBabelTransform.ts
+++ packages/sku/src/config/jest/jsBabelTransform.ts
@@ -2,15 +2,17 @@ import { createTransformer } from 'babel-jest';
 
 import babelConfig from '../babel/babelConfig.js';
 import targets from '../targets.json' with { type: 'json' };
+
 import { getSkuContext } from '@/context/createSkuContext.js';
 
 const { rootResolution } = getSkuContext();
 
-export default createTransformer(
+const transformer: ReturnType<typeof createTransformer> = createTransformer(
   babelConfig({
     target: 'jest',
-    lang: 'ts',
     rootResolution,
     browserslist: targets.browserslistNodeTarget,
   }),
 );
+
+export default transformer;

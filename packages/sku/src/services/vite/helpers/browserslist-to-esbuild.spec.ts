8ad61eae1e8c1ce4740d6e67d0ce8bf0ca27b310
Dan Drory <101684796+DanDroryAu@users.noreply.github.com>
2025-07-09 09:47:02 +1000

    enable supportedBrowsers when running vite (#1298)
    Co-authored-by: Adam Skoufis <askoufis@users.noreply.github.com>

---
+++ packages/sku/src/services/vite/helpers/browserslist-to-esbuild.spec.ts
@@ -0,0 +1,77 @@
+import { test } from 'vitest';
+import browserslistToEsbuild from './browserslist-to-esbuild.js';
+
+test('works by passing browsers as array', ({ expect }) => {
+  const target = browserslistToEsbuild([
+    '>0.2%',
+    'not dead',
+    'not op_mini all',
+  ]);
+
+  expect(target).toMatchInlineSnapshot(`
+    [
+      "chrome109",
+      "edge135",
+      "firefox138",
+      "ios11",
+      "safari17.6",
+    ]
+  `);
+});
+
+test('works by passing browsers as string', ({ expect }) => {
+  const target = browserslistToEsbuild('last 2 versions');
+
+  expect(target).toMatchInlineSnapshot(`
+    [
+      "chrome137",
+      "edge136",
+      "firefox139",
+      "ie10",
+      "ios18.4",
+      "opera116",
+      "safari18.4",
+    ]
+  `);
+});
+
+test('works with ios', ({ expect }) => {
+  const target = browserslistToEsbuild('ios >= 9');
+
+  expect(target).toMatchInlineSnapshot(`
+    [
+      "ios9",
+    ]
+  `);
+});
+
+test('works with android and ios', ({ expect }) => {
+  const target = browserslistToEsbuild('ios >= 11, android >= 5');
+
+  expect(target).toMatchInlineSnapshot(`
+    [
+      "chrome137",
+      "ios11",
+    ]
+  `);
+});
+
+test('no support for android 4', ({ expect }) => {
+  const target = browserslistToEsbuild('android >= 4');
+
+  expect(target).toMatchInlineSnapshot(`
+    [
+      "chrome137",
+    ]
+  `);
+});
+
+test('safari TP defaults to latest safari', ({ expect }) => {
+  const target = browserslistToEsbuild('safari TP');
+
+  expect(target).toMatchInlineSnapshot(`
+    [
+      "safari18.5",
+    ]
+  `);
+});

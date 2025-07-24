8e8d0b5415d14c8a815856b938a75429f5eb6d85
Adam Skoufis <askoufis@seek.com.au>
2025-07-25 09:36:43 +1000

    Add comment about PNPM CLI bug

--- packages/sku/src/services/packageManager/getPnpmConfigDependencies.ts
+++ packages/sku/src/services/packageManager/getPnpmConfigDependencies.ts
@@ -1,6 +1,9 @@
 import { execAsync } from '@/utils/execAsync.js';
 
 export const getPnpmConfigDependencies = async (): Promise<string[]> => {
+  // `pnpm config get configDependencies` returns `[object Object]` for some reason, so we fetch the
+  // entire config as JSON and parse it instead.
+  // See https://github.com/pnpm/pnpm/issues/9797
   const rawResult = await execAsync(`pnpm config get --json`);
   if (!rawResult) {
     return [];

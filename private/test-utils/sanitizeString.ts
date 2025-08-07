fe91bc4df913b429dcca03122524cc31aa4d58db
William Lark <wlark@seek.com.au>
2025-08-07 16:28:29 +1000

    updating sanitisation

--- private/test-utils/sanitizeString.ts
+++ private/test-utils/sanitizeString.ts
@@ -1,8 +1,11 @@
 export function sanitizeString(str: string) {
   const sanitized = str
     .replaceAll(process.cwd(), '{cwd}')
-    // Sanitize .pnpm paths to avoid test failures on different machines
-    .replaceAll(/\.pnpm\/[^/]+@[^/]+_[^/]+/g, '.pnpm/{package}')
+    // Sanitize .pnpm paths to make snapshots more readable
+    .replaceAll(
+      /node_modules\/\.pnpm\/[^/]+@[^/]+_[^/]+\/node_modules/g,
+      '{node_modules}',
+    )
     // replace 8-character hashes between dash and file extension.
     // This will reduce changes in snapshots when dependency hashes (vite, braid, etc.) change.
     .replaceAll(/-.{8}\.js(\.map)?/g, '-{hash}.js$1');

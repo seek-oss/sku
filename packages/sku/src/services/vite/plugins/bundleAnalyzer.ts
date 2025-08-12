48b8926c63506a16a95a94cfdc377b2546948c4a
Aden Margheriti <amargheriti@seek.com.au>
2025-08-12 11:53:34 +1000

    tweak plugin config options

--- packages/sku/src/services/vite/plugins/bundleAnalyzer.ts
+++ packages/sku/src/services/vite/plugins/bundleAnalyzer.ts
@@ -1,14 +1,15 @@
-import { getPathFromCwd } from '../../../utils/cwd.js';
+import { getPathFromCwd, requireFromCwd } from '../../../utils/cwd.js';
 import { visualizer } from 'rollup-plugin-visualizer';
 import type { Plugin } from 'vite';
 
 export const bundleReportFolder = 'report';
 
+const getProjectName = (): string => requireFromCwd('./package.json').name;
+
 export const bundleAnalyzerPlugin = ({ name }: { name: string }): Plugin =>
   visualizer({
     filename: getPathFromCwd(`${bundleReportFolder}/${name}.html`),
     template: 'treemap',
-    open: false,
     gzipSize: true,
-    brotliSize: false,
+    title: getProjectName() || 'Vite Bundle Analyzer',
   });

b6592fd1e0cf88916c0eff3eed3dee356bd237ff
William Lark <wlark@seek.com.au>
2025-08-15 13:03:18 +1000

    reducing storybook test timeout

--- tests/browser/storybook-config.test.ts
+++ tests/browser/storybook-config.test.ts
@@ -25,7 +25,7 @@ import {
 const storybookStartedRegex =
   /Storybook \d+\.\d+\.\d+ for react-webpack5 started/;
 
-const timeout = 100_000;
+const timeout = 50_000;
 
 vi.setConfig({
   hookTimeout: timeout + 1000,

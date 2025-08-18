98f12bc6f5ab9bb0e732d8b14a564d4b15f557ae
William Lark <willjlark@gmail.com>
2025-08-18 12:48:04 +1000

    Vite v7 (#1346)

--- tests/node/sku-init.test.ts
+++ tests/node/sku-init.test.ts
@@ -6,9 +6,11 @@ import {
   expect as globalExpect,
   vi,
 } from 'vitest';
+import { parseDocument, Document } from 'yaml';
 
 import fs from 'node:fs/promises';
 import { scopeToFixture } from '@sku-private/testing-library';
+import path from 'node:path';
 
 const { sku, fixturePath } = scopeToFixture('sku-init');
 
@@ -22,18 +24,35 @@ vi.setConfig({
   testTimeout: timeout + 1000,
 });
 
+/**
+ * Creates a new workspace yaml with some values copied from the root workspace yaml.
+ * This allows us to use the same catalog as the root workspace.
+ */
+const createWorkspace = async () => {
+  // Get the root pnpm-workspace.yaml
+  const rootFile = await fs.readFile(
+    path.resolve(__dirname, '../../pnpm-workspace.yaml'),
+    'utf8',
+  );
+  const rootYaml = parseDocument(rootFile);
+  const catalog = rootYaml.get('catalog');
+
+  // Create a new workspace yaml and add the needed fields
+  const newWorkspaceYaml = new Document();
+  newWorkspaceYaml.set('catalog', catalog);
+  newWorkspaceYaml.set('packages', ['../../packages/*']);
+  newWorkspaceYaml.set('linkWorkspacePackages', true);
+
+  return newWorkspaceYaml.toString();
+};
+
 describe('sku init', () => {
   beforeAll(async () => {
     await fs.rm(projectDirectory, { recursive: true, force: true });
-    await fs.writeFile(
-      fixturePath('pnpm-workspace.yaml'),
-      `
-      packages:
-        - ../../packages/*
 
-      linkWorkspacePackages: true
-    `,
-    );
+    const workspace = await createWorkspace();
+
+    await fs.writeFile(fixturePath('pnpm-workspace.yaml'), workspace);
 
     const result = await sku('init', [projectName]);
     globalExpect(
@@ -69,12 +88,17 @@ describe('sku init', () => {
   });
 
   it('should update the pnpm-workspace.yaml', async ({ expect }) => {
-    const contents = await fs.readFile(
-      fixturePath('pnpm-workspace.yaml'),
-      'utf-8',
+    const rootFile = await fs.readFile(
+      path.resolve(fixturePath('pnpm-workspace.yaml')),
+      'utf8',
     );
+    const workspace = parseDocument(rootFile);
+    // Delete some of the fields that we don't care about
+    workspace.delete('catalog');
+    workspace.delete('packages');
+    workspace.delete('linkWorkspacePackages');
 
-    expect(stripYamlVersions(contents)).toMatchSnapshot();
+    expect(stripYamlVersions(workspace.toString())).toMatchSnapshot();
   });
 });
 

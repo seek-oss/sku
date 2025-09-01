9c2e016313d588a6a99391516375cd4767377b0f
Aden <account.github@adenj.com>
2025-09-01 13:38:02 +1000

    `create`: Add minimal sku-create CLI package (#1366)

---
+++ packages/create/src/cli.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import prompts from 'prompts';
+
+import packageJson from '@sku-lib/create/package.json' with { type: 'json' };
+
+const { name, description, version } = packageJson;
+
+const program = new Command();
+
+program
+  .name(name)
+  .description(description)
+  .version(version)
+  .argument('[project-name]', 'Name of the project to create')
+  .option('-t, --template <template>', 'Template to use (webpack, vite)')
+  .action(async (projectName: string = '.', options: { template?: string }) => {
+    let selectedTemplate = options.template;
+
+    if (!selectedTemplate) {
+      const { template } = await prompts({
+        type: 'select',
+        name: 'template',
+        message: 'Which template would you like to use?',
+        choices: [
+          { title: 'webpack', value: 'webpack' },
+          { title: 'vite', value: 'vite' },
+        ],
+        initial: 0,
+      });
+
+      selectedTemplate = template;
+    }
+
+    console.log('🚧 create-sku is not yet implemented');
+    console.log(`Project name: ${projectName}`);
+    console.log(`Template: ${selectedTemplate}`);
+  });
+
+program.parse();

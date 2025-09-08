a8df1970a2f6149d82f6b9b4434faebb3d4564a0
Adam Skoufis <askoufis@seek.com.au>
2025-09-09 09:54:01 +1000

    Tweak wording

--- packages/create/src/cli.ts
+++ packages/create/src/cli.ts
@@ -54,13 +54,13 @@ program
             title: 'Webpack',
             value: 'webpack',
             description:
-              "Equivalent to the existing 'sku init' template. Uses Webpack for serving and bundling your application, and Jest for running tests. Select this template for a stable, battle-tested development environment.",
+              "Equivalent to the existing 'sku init' template. Uses Webpack for serving and bundling your application, and Jest for running tests. Select this template for a familiar, production-ready development environment.",
           },
           {
             title: 'Vite (experimental)',
             value: 'vite',
             description:
-              'NOT PRODUCTION-READY. An experimental template that uses Vite for serving and bundling your application, and Vitest for running tests. Select this template for fast builds and lightning-fast local development.',
+              'NOT PRODUCTION-READY. An experimental template that uses Vite for serving and bundling your application, and Vitest for running tests. Select this template to experiment with upcoming features.',
           },
         ],
         initial: 0,
@@ -74,7 +74,7 @@ program
         type: 'confirm',
         name: 'confirmVite',
         message:
-          '⚠️  Vite support in sku is currently experimental and not yet suitable for production use. Continue?',
+          '⚠️ Vite support in sku is currently experimental and not yet suitable for production use. Continue?',
         initial: false,
       });
 

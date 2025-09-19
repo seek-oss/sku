852868a617575ddb5cea2c4c49e25df98b7a421e
Aden Margheriti <account.github@adenj.com>
2025-09-19 10:35:54 +1000

    refine wording in registry reasons

--- packages/sku/src/utils/polyfillRegistry.ts
+++ packages/sku/src/utils/polyfillRegistry.ts
@@ -11,7 +11,7 @@ export type PolyfillRegistry = Record<string, PolyfillRegistryEntry>;
 export const POLYFILL_REGISTRY: PolyfillRegistry = {
   'core-js': {
     reason:
-      'Polyfills many features that may be unnecessary. Consider removing or using targeted polyfills for specific APIs',
+      'Polyfills many features that may be unnecessary. Consider removing or using targeted polyfills for specific APIs.',
     docsUrl: 'https://github.com/zloirock/core-js#usage',
   },
   '@babel/polyfill': {
@@ -24,24 +24,30 @@ export const POLYFILL_REGISTRY: PolyfillRegistry = {
   },
   'regenerator-runtime': {
     reason:
-      'async/await is well established and works across many devices and browser versions',
+      'async/await is well established and works across many devices and browser versions. It has been available across browsers since April 2017.',
   },
   'es6-promise': {
-    reason: 'Promises are natively supported in modern browsers',
+    reason:
+      'Promises is well established and works across many devices and browser versions. It has been available across browsers since July 2015.',
   },
   'es6-promise/auto': {
-    reason: 'Promises are natively supported in modern browsers',
+    reason:
+      'Promises is well established and works across many devices and browser versions. It has been available across browsers since July 2015.',
   },
   'promise-polyfill': {
-    reason: 'Promises are natively supported in modern browsers',
+    reason:
+      'Promises is well established and works across many devices and browser versions. It has been available across browsers since July 2015.',
   },
   'whatwg-fetch': {
-    reason: 'Fetch API is natively supported in modern browsers',
+    reason:
+      'Fetch API is well established and works across many devices and browser versions. It has been available across browsers since March 2017.',
   },
   'isomorphic-fetch': {
-    reason: 'Fetch API is natively supported in modern browsers',
+    reason:
+      'Fetch API is well established and works across many devices and browser versions. It has been available across browsers since March 2017, and Node.js since April 2022.',
   },
   'intersection-observer': {
-    reason: 'IntersectionObserver is natively supported in modern browsers',
+    reason:
+      'IntersectionObserver is well established and works across many devices and browser versions. It has been available across browsers since March 2019.',
   },
 } as const;

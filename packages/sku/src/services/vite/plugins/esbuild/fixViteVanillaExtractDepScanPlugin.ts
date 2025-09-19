b4267b5746133fbcdc7518fd8b8350677cd2f8be
Adam Skoufis <askoufis@seek.com.au>
2025-09-19 10:50:52 +1000

    Replace `resolve-from` dependency with native `require.resolve`

--- packages/sku/src/services/vite/plugins/esbuild/fixViteVanillaExtractDepScanPlugin.ts
+++ packages/sku/src/services/vite/plugins/esbuild/fixViteVanillaExtractDepScanPlugin.ts
@@ -1,13 +1,15 @@
 import type { Plugin } from 'esbuild';
 import { cssFileFilter } from '@vanilla-extract/integration';
-import resolveFrom from 'resolve-from';
 import { dirname } from 'node:path';
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
 
 export const fixViteVanillaExtractDepScanPlugin = (): Plugin => ({
   name: 'fix-vite-vanilla-extract-dep-scan',
   setup(build) {
-    build.onResolve({ filter: cssFileFilter }, async ({ importer, path }) => ({
-      path: resolveFrom(dirname(importer), path),
+    build.onResolve({ filter: cssFileFilter }, ({ importer, path }) => ({
+      path: require.resolve(path, { paths: [dirname(importer)] }),
       external: true,
     }));
   },

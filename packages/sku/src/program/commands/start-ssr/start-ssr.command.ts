69f6ae2b4d4185015b73231a771f60057007fc46
William Lark <wlark@seek.com.au>
2025-10-16 16:08:57 +1100

    enable list-urls for start-ssr

--- packages/sku/src/program/commands/start-ssr/start-ssr.command.ts
+++ packages/sku/src/program/commands/start-ssr/start-ssr.command.ts
@@ -1,11 +1,13 @@
 import { Command } from 'commander';
 import { statsOption } from '../../options/stats.option.js';
+import { listUrlsOption } from '../../options/list-urls.js';
 
 export const startSsrCommand = new Command('start-ssr')
   .description(
     'Start the sku development server for a server-rendered application.',
   )
   .addOption(statsOption)
+  .addOption(listUrlsOption)
   .action(async (options) => {
     const { startSsrAction } = await import('./start-ssr.action.js');
     await startSsrAction(options);

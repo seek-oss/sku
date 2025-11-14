d09635e452ac9bbd28d4712c6264f8c01d9bca5c
William Lark <wlark@seek.com.au>
2025-11-14 14:07:56 +1100

    adding changeset and bumping slowmo

--- private/playwright/browser.ts
+++ private/playwright/browser.ts
@@ -16,7 +16,7 @@ const getBrowser = async () => {
   log('Launching browser');
   _browser = chromium.launch({
     // Slow down Playwright actions so the browser has time to hydrate the app
-    slowMo: 50,
+    slowMo: 100,
   });
 
   return _browser;

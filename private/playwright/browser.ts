b8d8cce99c4c87965a871b6c131c05d8d857b929
William Lark <wlark@seek.com.au>
2025-11-21 13:41:40 +1100

    fixing multiple routes test

--- private/playwright/browser.ts
+++ private/playwright/browser.ts
@@ -16,7 +16,7 @@ const getBrowser = async () => {
   log('Launching browser');
   _browser = chromium.launch({
     // Slow down Playwright actions so the browser has time to hydrate the app
-    slowMo: 100,
+    slowMo: 50,
   });
 
   return _browser;

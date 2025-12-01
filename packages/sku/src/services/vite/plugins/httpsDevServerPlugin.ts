411d2f1bd36f320cde074de551f975ffe8556aba
Adam Skoufis <askoufis@users.noreply.github.com>
2025-12-01 15:18:18 +1100

    `vite`: Enable local HTTPS dev servers to use the HTTP/2 protocol (#1462)

--- packages/sku/src/services/vite/plugins/httpsDevServerPlugin.ts
+++ packages/sku/src/services/vite/plugins/httpsDevServerPlugin.ts
@@ -4,18 +4,6 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 import path from 'node:path';
 import type { Plugin } from 'vite';
 
-/**
- * Force HTTP/1.1 for the dev server. This is needed for proxy middleware that does not support HTTP/2.
- */
-const forceHttp1 = (): Plugin => ({
-  name: 'sku:force-http1',
-  config: () => ({
-    server: {
-      proxy: {},
-    },
-  }),
-});
-
 export const httpsDevServerPlugin = (skuContext: SkuContext): Plugin[] => {
   if (!skuContext.httpsDevServer) {
     return [];
@@ -23,7 +11,6 @@ export const httpsDevServerPlugin = (skuContext: SkuContext): Plugin[] => {
   const certDir = path.join(process.cwd(), '.ssl');
 
   return [
-    forceHttp1(),
     basicSsl({
       domains: getAppHosts(skuContext).filter((host) => host !== undefined),
       certDir,

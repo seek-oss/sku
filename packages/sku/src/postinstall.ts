fd98cd66b379a89096f610a376d033807b79f62d
William Lark <wlark@seek.com.au>
2025-12-02 15:25:40 +1100

    fixing lint errors

---
+++ packages/sku/src/postinstall.ts
@@ -0,0 +1,46 @@
+import debug from 'debug';
+import { setCwd, banner } from '@sku-lib/utils';
+import { createSkuContext } from './context/createSkuContext.js';
+import { styleText } from 'node:util';
+import configureApp from './utils/configureApp.js';
+
+/**
+ * Separated out from the scripts/postinstall.js so that this can be bundled with vite
+ */
+
+const log = debug('sku:postinstall');
+const bold = (text: string) => styleText(['bold'], text);
+
+export const postinstall = async ({
+  localCwd,
+  packageJson,
+  hasSkuDep,
+}: {
+  localCwd: string;
+  packageJson: string;
+  hasSkuDep: boolean;
+}) => {
+  setCwd(localCwd);
+
+  if (hasSkuDep) {
+    banner('warning', 'sku dependency detected', [
+      `${bold('sku')} is installed as a ${bold('dependency')} in ${bold(
+        packageJson,
+      )}.`,
+      `${bold('sku')} should be installed in ${bold('devDependencies')}.`,
+    ]);
+  }
+
+  try {
+    log('postinstall', 'running configure');
+    const skuContext = createSkuContext({});
+    configureApp(skuContext);
+  } catch (error) {
+    console.error(
+      'An error occurred running postinstall script. Please check that sku.config.js is correct and try again.',
+    );
+    console.error(error);
+    process.exit(1);
+  }
+  log('postinstall', 'successfully configured');
+};
--- packages/sku/tsdown.config.ts
+++ packages/sku/tsdown.config.ts
@@ -1,38 +1,47 @@
 import { defineConfig } from 'tsdown';
 import { defaultConfig } from '@sku-private/tsdown';
 
-export default defineConfig({
-  ...defaultConfig,
-  // Need to use unbundled mode because `webpack/entry/server/index.ts` calls webpackHot.accept which needs the server app to be a different file.
-  unbundle: true,
-  entry: {
-    index: 'src/index.ts',
-    '@loadable/component': 'src/@loadable/component/index.ts',
-    'bin/sku': 'src/bin/sku.ts',
-    'config/eslint': 'src/config/eslint/config.ts',
-    'config/prettier': 'src/config/prettier.ts',
-    'config/storybook': 'src/config/storybook/config.cjs',
-    'entries/vite-client': 'src/services/vite/entries/vite-client.tsx',
-    'entries/vite-render': 'src/services/vite/entries/vite-render.tsx',
-    'jest-preset': 'src/config/jest/preset.ts',
-    'jest/file-mock': 'src/config/jest/fileMock.cjs',
-    'jest/js-transform': 'src/config/jest/jsBabelTransform.ts',
-    'jest/ts-transform': 'src/config/jest/tsBabelTransform.ts',
-    'vite/client': 'src/services/vite/index.ts',
-    'vite/prerender-worker':
-      'src/services/vite/helpers/prerender/prerenderWorker.ts',
-    'webpack-plugin': 'src/services/webpack/config/plugins/skuWebpackPlugin.ts',
-    'webpack/client': 'src/services/webpack/entry/client/index.ts',
-    'webpack/library-render':
-      'src/services/webpack/entry/libraryRender/index.ts',
-    'webpack/render': 'src/services/webpack/entry/render/index.ts',
-    'webpack/server': 'src/services/webpack/entry/server/index.ts',
+export default defineConfig([
+  {
+    ...defaultConfig,
+    // Need to use unbundled mode because `webpack/entry/server/index.ts` calls webpackHot.accept which needs the server app to be a different file.
+    unbundle: true,
+    entry: {
+      index: 'src/index.ts',
+      '@loadable/component': 'src/@loadable/component/index.ts',
+      'bin/sku': 'src/bin/sku.ts',
+      'config/eslint': 'src/config/eslint/config.ts',
+      'config/prettier': 'src/config/prettier.ts',
+      'config/storybook': 'src/config/storybook/config.cjs',
+      'entries/vite-client': 'src/services/vite/entries/vite-client.tsx',
+      'entries/vite-render': 'src/services/vite/entries/vite-render.tsx',
+      'jest-preset': 'src/config/jest/preset.ts',
+      'jest/file-mock': 'src/config/jest/fileMock.cjs',
+      'jest/js-transform': 'src/config/jest/jsBabelTransform.ts',
+      'jest/ts-transform': 'src/config/jest/tsBabelTransform.ts',
+      'vite/client': 'src/services/vite/index.ts',
+      'vite/prerender-worker':
+        'src/services/vite/helpers/prerender/prerenderWorker.ts',
+      'webpack-plugin':
+        'src/services/webpack/config/plugins/skuWebpackPlugin.ts',
+      'webpack/client': 'src/services/webpack/entry/client/index.ts',
+      'webpack/library-render':
+        'src/services/webpack/entry/libraryRender/index.ts',
+      'webpack/render': 'src/services/webpack/entry/render/index.ts',
+      'webpack/server': 'src/services/webpack/entry/server/index.ts',
+    },
+    external: [
+      '__sku_alias__renderEntry',
+      '__sku_alias__clientEntry',
+      '__sku_alias__serverEntry',
+      '__sku_alias__webpackStats',
+      'virtual:sku/polyfills',
+      '@vanilla-extract/css/adapter',
+    ],
   },
-  external: [
-    '__sku_alias__renderEntry',
-    '__sku_alias__clientEntry',
-    '__sku_alias__serverEntry',
-    '__sku_alias__webpackStats',
-    'virtual:sku/polyfills',
-  ],
-});
+  {
+    ...defaultConfig,
+    exports: false,
+    entry: './src/postinstall.ts',
+  },
+]);

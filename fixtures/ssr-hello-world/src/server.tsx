3e5747187aee6004adc650c11e127d99aafd65ab
William Lark <wlark@seek.com.au>
2026-02-24 17:44:51 +1100

    satisfy Server config

--- fixtures/ssr-hello-world/src/server.tsx
+++ fixtures/ssr-hello-world/src/server.tsx
@@ -1,4 +1,5 @@
 import { renderToString } from 'react-dom/server';
+import type { Server } from 'sku';
 
 import App from './App';
 
@@ -23,44 +24,45 @@ const template = ({ headTags, bodyTags, app, extraScripts }) => /* html */ `
   </html>
 `;
 
-export default () => ({
-  renderCallback: async (
-    { SkuProvider, getBodyTags, flushHeadTags, registerScript },
-    req,
-    res,
-  ) => {
-    const extraScripts = [
-      `<script src="https://code.jquery.com/jquery-3.5.0.slim.min.js"></script>`,
-      `<script>console.log('Hi');</script>`,
-    ];
+export default () =>
+  ({
+    renderCallback: async (
+      { SkuProvider, getBodyTags, flushHeadTags, registerScript },
+      req,
+      res,
+    ) => {
+      const extraScripts = [
+        `<script src="https://code.jquery.com/jquery-3.5.0.slim.min.js"></script>`,
+        `<script>console.log('Hi');</script>`,
+      ];
 
-    extraScripts.forEach((script) => registerScript(script));
+      extraScripts.forEach((script) => registerScript?.(script));
 
-    res.status(200).write(
-      initialResponseTemplate({
-        headTags: flushHeadTags(),
-      }),
-    );
-    await Promise.resolve();
+      res.status(200).write(
+        initialResponseTemplate({
+          headTags: flushHeadTags(),
+        }),
+      );
+      await Promise.resolve();
 
-    const app = renderToString(
-      <SkuProvider>
-        <App />
-      </SkuProvider>,
-    );
-    res.write(
-      template({
-        headTags: flushHeadTags(),
-        bodyTags: getBodyTags(),
-        app,
-        extraScripts,
-      }),
-    );
-    res.end();
-  },
-  onStart: async () => {
-    if (process.env.NODE_ENV === 'production') {
-      console.log('Server ran the onStart callback');
-    }
-  },
-});
+      const app = renderToString(
+        <SkuProvider>
+          <App />
+        </SkuProvider>,
+      );
+      res.write(
+        template({
+          headTags: flushHeadTags(),
+          bodyTags: getBodyTags(),
+          app,
+          extraScripts,
+        }),
+      );
+      res.end();
+    },
+    onStart: async () => {
+      if (process.env.NODE_ENV === 'production') {
+        console.log('Server ran the onStart callback');
+      }
+    },
+  }) satisfies Server;

b6f4bd11e7b7e116cfeaa0d9b7de1a49fe397fdf
Adam Skoufis <askoufis@seek.com.au>
2026-03-10 15:24:17 +1100

    Add fixtures and tests

---
+++ fixtures/vite-cjs-interop-apollo-client-v3/src/App.tsx
@@ -0,0 +1,14 @@
+import { gql } from '@apollo/client';
+import { useQuery } from '@apollo/client/react';
+
+const query = gql`
+  {
+    foo
+  }
+`;
+
+export default () => {
+  const _result = useQuery(query, { skip: true });
+
+  return <div>Vite CJS Interop - Apollo Client v3</div>;
+};

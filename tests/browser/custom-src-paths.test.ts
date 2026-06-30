7df5ed5df6b5ad8164e1f31111ef8dc2f7159074
William Lark <wlark@seek.com.au>
2026-06-30 16:50:15 +1000

    testing srcPaths failing

--- tests/browser/custom-src-paths.test.ts
+++ tests/browser/custom-src-paths.test.ts
@@ -3,86 +3,80 @@ import { dirContentsToObject, getPort } from '@sku-private/test-utils';
 
 import { getAppSnapshot } from '@sku-private/playwright';
 
-import {
-  bundlers,
-  type BundlerValues,
-  skipCleanup,
-  scopeToFixture,
-} from '@sku-private/testing-library';
+import { skipCleanup, scopeToFixture } from '@sku-private/testing-library';
 
 const { sku, fixturePath } = scopeToFixture('custom-src-paths');
 
+/**
+ * srcPaths is a webpack-only field, so only running webpack tests.
+ */
+
 describe('custom-src-paths', () => {
-  describe.for(bundlers)('bundler %s', (bundler) => {
-    describe('start', async () => {
-      it('should start a development server', async () => {
-        const port = await getPort();
-        const url = `http://localhost:${port}`;
-
-        const args: BundlerValues<string[]> = {
-          vite: [
-            '--config',
-            'sku.config.vite.ts',
-            '--strict-port',
-            `--port=${port}`,
-          ],
-          webpack: ['--strict-port', `--port=${port}`],
-        };
-
-        const start = await sku('start', args[bundler]);
-        expect(
-          await start.findByText('Starting development server'),
-        ).toBeInTheConsole();
-
-        const snapshot = await getAppSnapshot({ url });
-        expect(snapshot).toMatchSnapshot();
-      });
-    });
-
-    describe('build', async () => {
+  describe('start', async () => {
+    it('should start a development server', async () => {
       const port = await getPort();
       const url = `http://localhost:${port}`;
-      const portArgs = ['--strict-port', `--port=${port}`];
 
-      const args: BundlerValues<string[]> = {
-        vite: ['--config', 'sku.config.vite.ts'],
-        webpack: [],
-      };
+      const start = await sku('start', ['--strict-port', `--port=${port}`]);
+      expect(
+        await start.findByText('Starting development server'),
+      ).toBeInTheConsole();
 
-      beforeAll(async () => {
-        const build = await sku('build', args[bundler]);
-        await build.findByText('Build complete');
+      const snapshot = await getAppSnapshot({ url });
+      expect(snapshot).toMatchSnapshot();
+    });
 
-        const serve = await sku('serve', portArgs);
-        await serve.findByText('Server started');
-      });
-
-      it('should generate the expected files', async ({ task }) => {
-        skipCleanup(task.id);
-        const files = await dirContentsToObject(fixturePath('dist'));
-        expect(files).toMatchSnapshot();
-      });
-
-      it('should create valid app', async ({ task }) => {
-        skipCleanup(task.id);
-
-        const app = await getAppSnapshot({ url });
-        expect(app).toMatchSnapshot();
-      });
+    it('should fail if `srcPaths` is missing values', async () => {
+      const start = await sku('start', ['--config=sku.config.bad.ts']);
+      expect(
+        await start.findByText(
+          // if `srcPaths` values are missing then the files won't be passed to webpack,
+          // leading to webpack not knowing how to process the file (in the case of .ts files at least).
+          // This is the standard error message for webpack when that happens.
+          'You may need an appropriate loader to handle this file type',
+        ),
+      ).toBeInTheConsole();
     });
   });
 
-  describe('format', () => {
-    it('should format successfully', async () => {
-      const format = await sku('format');
-      expect(await format.findByText('Formatting complete')).toBeInTheConsole();
+  describe('build', async () => {
+    const port = await getPort();
+    const url = `http://localhost:${port}`;
+    const portArgs = ['--strict-port', `--port=${port}`];
+
+    beforeAll(async () => {
+      const build = await sku('build');
+      await build.findByText('Build complete');
+
+      const serve = await sku('serve', portArgs);
+      await serve.findByText('Server started');
+    });
+
+    it('should generate the expected files', async ({ task }) => {
+      skipCleanup(task.id);
+      const files = await dirContentsToObject(fixturePath('dist'));
+      expect(files).toMatchSnapshot();
+    });
+
+    it('should create valid app', async ({ task }) => {
+      skipCleanup(task.id);
+
+      const app = await getAppSnapshot({ url });
+      expect(app).toMatchSnapshot();
     });
   });
+});
 
-  describe('lint', () => {
-    it('should lint successfully', async () => {
-      const lint = await sku('lint');
-      expect(await lint.findByText('Linting complete')).toBeInTheConsole();
-    });
+describe('format', () => {
+  it('should format successfully', async () => {
+    const format = await sku('format');
+    expect(await format.findByText('Formatting complete')).toBeInTheConsole();
+  });
+});
+
+describe('lint', () => {
+  it('should lint successfully', async () => {
+    const lint = await sku('lint');
+    expect(await lint.findByText('Linting complete')).toBeInTheConsole();
   });
 });

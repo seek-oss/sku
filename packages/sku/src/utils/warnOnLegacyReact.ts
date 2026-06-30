785796152d07ff19bc099e6e793a321dde929914
William Lark <wlark@seek.com.au>
2026-06-30 15:57:34 +1000

    Adding legacy react warning (#1603)
    Co-authored-by: Adam Skoufis <askoufis@users.noreply.github.com>

---
+++ packages/sku/src/utils/warnOnLegacyReact.ts
@@ -0,0 +1,23 @@
+import { requireFromCwd } from '@sku-private/utils';
+import { banner, link, strong } from '@sku-private/utils/console';
+import semver from 'semver';
+import _debug from 'debug';
+
+const MIN_REACT_VERSION = '19.0.0';
+
+const debug = _debug('sku:warnOnLegacyReact');
+
+export const warnOnLegacyReact = () => {
+  try {
+    const { version: reactVersion } = requireFromCwd('react/package.json');
+    const coerced = semver.coerce(reactVersion);
+    if (coerced && semver.lt(coerced, MIN_REACT_VERSION)) {
+      banner('caution', 'React 18 or below detected', [
+        `Your application is using ${strong(`react@${reactVersion}`)}. Sku now recommends using React 19.`,
+        `Consider upgrading to React 19. See ${link('https://react.dev/blog/2024/04/25/react-19-upgrade-guide')} for more information.`,
+      ]);
+    }
+  } catch {
+    debug('React not found, skipping warning');
+  }
+};

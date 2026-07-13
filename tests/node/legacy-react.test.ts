a2b680d3ca064a76636fb23edd87d37f73e82684
Adam Skoufis <askoufis@seek.com.au>
2026-07-13 10:41:25 +1000

    Install fake react 18 dependency

--- tests/node/legacy-react.test.ts
+++ tests/node/legacy-react.test.ts
@@ -1,11 +1,26 @@
 import { test, expect } from 'vitest';
 import { scopeToFixture } from '@sku-private/testing-library';
+import { mkdir, rmdir, writeFile } from 'node:fs/promises';
 
-const { sku } = scopeToFixture('legacy-react');
+const { sku, fixturePath } = scopeToFixture('legacy-react');
+
+const fakeReactPackageJson = {
+  name: 'react',
+  version: '18.0.0',
+};
 
 test('Legacy React should warn', async () => {
+  await mkdir(fixturePath('node_modules/react'), { recursive: true });
+  await writeFile(
+    fixturePath('node_modules/react/package.json'),
+    JSON.stringify(fakeReactPackageJson),
+    'utf8',
+  );
+
   const skuProcess = await sku('configure');
   expect(
     await skuProcess.findByText('React 18 or below detected'),
   ).toBeInTheConsole();
+
+  await rmdir(fixturePath('node_modules/react'), { recursive: true });
 });

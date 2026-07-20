3d9b44eeaab9f436ff32ca77f5fce06f6f94b55e
Karl Juri Ots <kjots@kjots.org>
2026-07-20 10:50:42 +1000

    Update app snapshot serializer to support HTTP headers.

--- private/playwright/appSnapshot.ts
+++ private/playwright/appSnapshot.ts
@@ -23,6 +23,7 @@ export const getAppSnapshot = async ({ url }: AppSnapshotOptions) => {
 
   const response = await page.goto(url);
 
+  const headers = (await response?.allHeaders()) ?? {};
   const sourceHtml = (await response?.text()) || '';
   const clientRenderContent = await page.content();
   await page.close();
@@ -30,5 +31,13 @@ export const getAppSnapshot = async ({ url }: AppSnapshotOptions) => {
   expect(errors).toEqual([]);
   expect(warnings).toEqual([]);
 
-  return { sourceHtml, clientRenderContent };
+  return { headers, sourceHtml, clientRenderContent };
 };
+
+export type AppSnapshot = Awaited<ReturnType<typeof getAppSnapshot>>;
+export const isAppSnapshot = (value: unknown): value is AppSnapshot =>
+  typeof value === 'object' &&
+  value !== null &&
+  'headers' in value &&
+  'sourceHtml' in value &&
+  'clientRenderContent' in value;

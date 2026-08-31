7a54b4bd05bedd6a4081a243767c4afb6c59fb0e
Adam Skoufis <askoufis@users.noreply.github.com>
2026-08-31 12:02:19 +1000

    Fix security-controls SSR fixture so client assets load from http-server (#1705)

--- fixtures/security-controls/sku-server.config.ts
+++ fixtures/security-controls/sku-server.config.ts
@@ -5,6 +5,8 @@ import {
 
 export default {
   port: 9847,
+  // Required for the test to serve client assets from http-server.
+  publicPath: 'http://localhost:9848',
   clientEntry: './src/serverClient.jsx',
   renderEntry: './src/render.jsx',
   serverEntry: './src/server.jsx',
